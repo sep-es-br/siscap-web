25e06d5ddce2ae600ea327ec8231361da41431a7
Artur Faria <artur.faria.1992@gmail.com>
2024-02-05 17:59:30 -0300

    ref: change route paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-//   {
-//     path: '',
-//     // component: AppComponent,
-//     redirectTo: 'login'
-//   },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: '**',
-    redirectTo: 'login',
+    redirectTo: '/login',
   },
 ];
