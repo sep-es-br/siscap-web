839313d7426807ee1a558fdaa81e25f73ddc212b
Artur Faria <artur.faria.1992@gmail.com>
2024-02-05 17:59:57 -0300

    feat: add login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../shared/services/authentication.service';
 
 @Component({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
 
+  navigateLoginGov() {
+    //Chamar authService com endpoint de verificação
+    //this.authService.authGovBr() ou this.authService.authAcessoCidadao()
+    this.authService.signIn();
+    // window.location.href = 'https://acessocidadao.es.gov.br/';
+  }
 }
