464cb479763047ca7c2d10e71faef3d9a4306714
Artur Faria <artur.faria.1992@gmail.com>
2024-02-07 17:50:50 -0300

    feat: add projects route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
@@ -17,7 +18,11 @@ export const routes: Routes = [
     component: HomeComponent,
   },
   {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
     path: '**',
-    redirectTo: '/home',
+    redirectTo: '',
   },
 ];
