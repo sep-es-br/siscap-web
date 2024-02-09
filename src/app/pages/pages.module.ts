33ba2b29b3fc93a42226452fa46428765f896764
Artur Faria <artur.faria.1992@gmail.com>
2024-02-09 08:08:05 -0300

    feat: add reactive forms module import

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,10 +4,17 @@ import { LoginComponent } from '../pages/login/login.component';
 import { HomeComponent } from '../pages/home/home.component';
 import { ComponentsModule } from '../components/components.module';
 import { ProjectsComponent } from './projects/projects.component';
+import { CreateComponent } from './projects/create/create.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [LoginComponent, HomeComponent, ProjectsComponent],
-  imports: [CommonModule, ComponentsModule],
-  exports: [LoginComponent, HomeComponent, ProjectsComponent],
+  declarations: [
+    LoginComponent,
+    HomeComponent,
+    ProjectsComponent,
+    CreateComponent,
+  ],
+  imports: [CommonModule, ComponentsModule, ReactiveFormsModule],
+  exports: [LoginComponent, HomeComponent, ProjectsComponent, CreateComponent],
 })
 export class PagesModule {}
