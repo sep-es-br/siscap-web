316916e77d6f912f31661ecf01707729e55eb2c8
Artur Faria <artur.faria.1992@gmail.com>
2024-02-09 08:00:42 -0300

    feat: add breadcrumb component

---
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumb',
+  standalone: false,
+  templateUrl: './breadcrumb.component.html',
+  styleUrl: './breadcrumb.component.css',
+})
+export class BreadcrumbComponent {}
