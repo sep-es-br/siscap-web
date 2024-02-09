e718e01fb2121f04e0fea7574ad942852478aed0
Artur Faria <artur.faria.1992@gmail.com>
2024-02-09 08:06:09 -0300

    feat: add breadcrumb component import

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavMenuComponent } from './navmenu/navmenu.component';
 import { HeaderComponent } from './header/header.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 @NgModule({
-  declarations: [NavMenuComponent, HeaderComponent],
+  declarations: [NavMenuComponent, HeaderComponent, BreadcrumbComponent],
   imports: [CommonModule],
-  exports: [NavMenuComponent, HeaderComponent],
+  exports: [NavMenuComponent, HeaderComponent, BreadcrumbComponent],
 })
 export class ComponentsModule {}
