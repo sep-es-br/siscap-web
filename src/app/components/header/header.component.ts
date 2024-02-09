29736f8653836a75351647ea024186b1b3b58fc6
Artur Faria <artur.faria.1992@gmail.com>
2024-02-09 10:33:29 -0300

    feat: add showMenu method

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor() {}
 
+  showMenu() {
+    const navMenuEl = document.getElementById('nav-menu')!.classList;
+    const responsiveContentEl =
+      document.getElementById('responsive-content')!.classList;
+
+    navMenuEl.remove('d-none');
+    responsiveContentEl.add('d-none');
+  }
 }
