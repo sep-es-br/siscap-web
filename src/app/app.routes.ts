a18b4d2a4bf5efaf439e99f6ce8e132702c09bf7
Artur Faria <artur.faria.1992@gmail.com>
2024-02-09 08:05:22 -0300

    feat: add project create route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { CreateComponent } from './pages/projects/create/create.component';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
   {
     path: 'projects',
     component: ProjectsComponent,
+  },
+  {
+    path: 'projects/create',
+    component: CreateComponent,
   },
   {
     path: '**',
