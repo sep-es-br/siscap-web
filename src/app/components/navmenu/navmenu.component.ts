983244fe5d328075e87e3ec8a4cc5e7c8bbb0c1f
Artur Faria <artur.faria.1992@gmail.com>
2024-02-09 10:29:28 -0300

    feat: add hideMenu method

--- src/app/components/navmenu/navmenu.component.ts
+++ src/app/components/navmenu/navmenu.component.ts
@@ -10,14 +10,21 @@ import { Router } from '@angular/router';
 export class NavMenuComponent {
   constructor(private _router: Router) {}
 
-
   /**
-  * Método para rotear o usuário para páginas do menu.
-  *
-  * @param {string} route - A rota para qual o usuário irá navegar.
-  * 
-  */
+   * Método para rotear o usuário para páginas do menu.
+   *
+   * @param {string} route - A rota para qual o usuário irá navegar.
+   *
+   */
   routerNavigation(route: string) {
     this._router.navigateByUrl(`/${route}`);
+  }
+
+  hideMenu() {
+    const navMenuEl = document.getElementById('nav-menu')!.classList;
+    const responsiveContentEl =
+      document.getElementById('responsive-content')!.classList;
+    navMenuEl.add('d-none');
+    responsiveContentEl.remove('d-none');
   }
 }
