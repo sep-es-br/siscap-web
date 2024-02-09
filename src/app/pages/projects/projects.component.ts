d78753793075a12ff049504b13140fb9f0027df3
Artur Faria <artur.faria.1992@gmail.com>
2024-02-09 08:11:54 -0300

    feat: change projects component

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: false,
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
+  constructor(private _router: Router) {}
 
+  redirectNewProject() {
+    this._router.navigateByUrl('/projects/create');
+  }
+
+  queryProject() {}
 }
