f80891d90556aca5b8d368db1c958d3eaefdc04d
Artur Faria <artur.faria.1992@gmail.com>
2024-02-15 16:20:17 -0300

    feat: add alert component

---
+++ src/app/components/alert/alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  standalone: false,
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.css',
+})
+export class AlertComponent {}
