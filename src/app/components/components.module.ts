7068c3c1a5e33fc43d2493e3e8e4eadda797b430
Artur Faria <artur.faria.1992@gmail.com>
2024-02-15 16:28:35 -0300

    feat: add AlertComponent declaration and export

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { NavMenuComponent } from './navmenu/navmenu.component';
 import { HeaderComponent } from './header/header.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
-  declarations: [NavMenuComponent, HeaderComponent, BreadcrumbComponent],
+  declarations: [
+    NavMenuComponent,
+    HeaderComponent,
+    BreadcrumbComponent,
+    AlertComponent,
+  ],
   imports: [CommonModule],
-  exports: [NavMenuComponent, HeaderComponent, BreadcrumbComponent],
+  exports: [
+    NavMenuComponent,
+    HeaderComponent,
+    BreadcrumbComponent,
+    AlertComponent,
+  ],
 })
 export class ComponentsModule {}
