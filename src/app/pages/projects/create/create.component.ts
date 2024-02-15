b6323c108d99ba75350e7b32bdaa9abd522a9385
Artur Faria <artur.faria.1992@gmail.com>
2024-02-15 16:37:14 -0300

    feat: add entidade, microrregiao service and logic; add documentation

--- src/app/pages/projects/create/create.component.ts
+++ src/app/pages/projects/create/create.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { Subscription, first } from 'rxjs';
+
+import { projectCreateModel } from '../../../shared/models/projectCreate.model';
+import { FormFactoryService } from '../../../shared/services/form-factory/form-factory.service';
+import { MicrorregiaoService } from '../../../shared/services/microrregiao/microrregiao.service';
+import { IMicrorregiao } from '../../../shared/interfaces/microrregiao.interface';
+import { EntidadeService } from '../../../shared/services/entidade/entidade.service';
+import { IEntidade } from '../../../shared/interfaces/entidade.interface';
+import { ProjetosService } from '../../../shared/services/projetos/projetos.service';
 
 @Component({
   selector: 'app-create',
@@ -7,73 +17,181 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './create.component.html',
   styleUrl: './create.component.css',
 })
-export class CreateComponent implements OnInit {
+export class CreateComponent implements OnInit, OnDestroy {
   public projectForm!: FormGroup;
-  // public projectCreateObj: projectCreateModel = {
-  //   sigla: 'asd',
-  //   titulo: 'teste',
-  //   idEntidade: 0,
-  //   valorEstimado: 0,
-  //   idMicrorregioes: [],
-  //   objetivo: '',
-  //   objetivoEspecifico: '',
-  //   situacaoProblema: '',
-  //   solucoesPropostas: '',
-  //   impactos: '',
-  //   arranjosInstitucionais: '',
-  // };
 
-  constructor(private _fb: FormBuilder) {}
+  private _microrregioes$!: Subscription;
+  private _entidades$!: Subscription;
+
+  public microrregioesList: IMicrorregiao[] = [];
+  public entidadesList: IEntidade[] = [];
+
+  private _projetos$!: Subscription;
+
+  public projectCreateObject: projectCreateModel = {
+    sigla: {
+      initialValue: '',
+      validators: {
+        required: true,
+        maxLength: 12,
+      },
+    },
+    titulo: {
+      initialValue: '',
+      validators: {
+        required: true,
+        maxLength: 150,
+      },
+    },
+    idEntidade: {
+      initialValue: 0,
+      validators: {
+        required: true,
+        min: 0,
+      },
+    },
+    valorEstimado: {
+      initialValue: 0,
+      validators: {
+        required: true,
+        min: 0,
+      },
+    },
+    idMicrorregioes: {
+      initialValue: [],
+      validators: {
+        required: true,
+      },
+    },
+    objetivo: {
+      initialValue: '',
+      validators: {
+        required: true,
+        maxLength: 2000,
+      },
+    },
+    objetivoEspecifico: {
+      initialValue: '',
+      validators: {
+        required: true,
+        maxLength: 2000,
+      },
+    },
+    situacaoProblema: {
+      initialValue: '',
+      validators: {
+        required: true,
+        maxLength: 2000,
+      },
+    },
+    solucoesPropostas: {
+      initialValue: '',
+      validators: {
+        required: true,
+        maxLength: 2000,
+      },
+    },
+    impactos: {
+      initialValue: '',
+      validators: {
+        required: true,
+        maxLength: 2000,
+      },
+    },
+    arranjosInstitucionais: {
+      initialValue: '',
+      validators: {
+        required: true,
+        maxLength: 2000,
+      },
+    },
+  };
+
+  constructor(
+    private _formFactory: FormFactoryService,
+    private _microrregiaoService: MicrorregiaoService,
+    private _entidadeService: EntidadeService,
+    private _projetosService: ProjetosService
+  ) {
+    this._microrregioes$ = this._microrregiaoService
+      .getMicrorregioes()
+      .pipe(first())
+      .subscribe((data) => {
+        this.microrregioesList = data;
+      });
+
+    this._entidades$ = this._entidadeService
+      .getEntidades()
+      .pipe(first())
+      .subscribe((data) => {
+        this.entidadesList = data;
+      });
+  }
 
   ngOnInit(): void {
     this.initForm();
-    console.log(this.projectForm.value);
   }
 
+  /**
+   * Método para inicialização do formulário.
+   *
+   */
   initForm() {
-    this.projectForm = this._fb.group({
-      sigla: this._fb.control('', [
-        Validators.required,
-        Validators.maxLength(12),
-      ]),
-      titulo: this._fb.control('', [
-        Validators.required,
-        Validators.maxLength(150),
-      ]),
-      idEntidade: this._fb.control(0, [Validators.required, Validators.min(0)]),
-      valorEstimado: this._fb.control(0, [
-        Validators.required,
-        Validators.min(0),
-      ]),
-      idMicrorregioes: this._fb.array([], Validators.required),
-      objetivo: this._fb.control('', [
-        Validators.required,
-        Validators.maxLength(2000),
-      ]),
-      objetivoEspecifico: this._fb.control('', [
-        Validators.required,
-        Validators.maxLength(2000),
-      ]),
-      situacaoProblema: this._fb.control('', [
-        Validators.required,
-        Validators.maxLength(2000),
-      ]),
-      solucoesPropostas: this._fb.control('', [
-        Validators.required,
-        Validators.maxLength(2000),
-      ]),
-      impactos: this._fb.control('', [
-        Validators.required,
-        Validators.maxLength(2000),
-      ]),
-      arranjosInstitucionais: this._fb.control('', [
-        Validators.required,
-        Validators.maxLength(2000),
-      ]),
-    });
+    this.projectForm = this._formFactory.generateForm(this.projectCreateObject);
   }
 
-  submitProjectCreateForm() {
-    console.log(this.projectForm.value);
+  /**
+   * Método para limpar o formulário.
+   *
+   */
+  resetForm() {
+    if (
+      confirm(
+        'O formulário será resetado para as configurações iniciais. Prosseguir?'
+      )
+    ) {
+      this.initForm();
+    }
+  }
+
+  /**
+   * Método para acessar os controles primariamente no template HTML do componente.
+   *
+   * @param {string} control - O nome do controle contido no objeto modelo, registrado através do FormFactoryService.
+   *
+   * @returns - O controle `AbstractControl`
+   */
+  getControl(control: string) {
+    return this.projectForm.get(control);
+  }
+
+  /**
+   * Método para criar um novo projeto no banco de dados.
+   *
+   * @param {FormGroup} payload - O `FormGroup` do formulário.
+   *
+   */
+  submitProjectCreateForm(payload: FormGroup) {
+    if (payload.invalid) {
+      alert('Formulário contém erros. Por favor verificar os campos.');
+      return;
+    }
+
+    //TODO: Tratamento de erro (caso sigla duplicada)
+    this._projetos$ = this._projetosService
+      .postProjetos(payload.value)
+      .subscribe((response) => {
+        console.log(response);
+        if (response.status == 201) {
+          alert('Projeto cadastrado com sucesso.');
+          window.history.go(-1);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._microrregioes$.unsubscribe();
+    this._entidades$.unsubscribe();
+    this._projetos$.unsubscribe();
   }
 }
