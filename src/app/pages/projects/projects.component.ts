fec4ee711ea1f5746b2ce9c7d4d47edd550bbe8c
Artur Faria <artur.faria.1992@gmail.com>
2024-02-15 16:33:16 -0300

    feat: add projetosService getProjetos method and logic

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+
+import { Subscription, first } from 'rxjs';
+
+import { ProjetosService } from '../../shared/services/projetos/projetos.service';
 
 @Component({
   selector: 'app-projects',
@@ -7,8 +11,23 @@ import { Router, RouterOutlet } from '@angular/router';
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
+//TODO: Tipar o array de projetos
 export class ProjectsComponent {
-  constructor(private _router: Router) {}
+  private _projetos$!: Subscription;
+
+  public projetosList: any[] = [];
+
+  constructor(
+    private _router: Router,
+    private _projetosService: ProjetosService
+  ) {
+    this._projetos$ = this._projetosService
+      .getProjetos()
+      .pipe(first())
+      .subscribe((data) => {
+        this.projetosList = data.content;
+      });
+  }
 
   redirectNewProject() {
     this._router.navigateByUrl('/projects/create');
