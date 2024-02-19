5d7386e139f86676bc928e5cef300d88cefc82b6
Artur Faria <artur.faria.1992@gmail.com>
2024-02-19 16:24:21 -0300

    feat: add loading spinner component

---
+++ src/app/components/loading/loading.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  standalone: false,
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.css',
+})
+export class LoadingComponent {}
