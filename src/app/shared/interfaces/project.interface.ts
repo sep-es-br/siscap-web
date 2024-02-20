18a37c0211f3c7b12d68956a8ebcbdc8e7e507fc
Artur Faria <artur.faria.1992@gmail.com>
2024-02-20 16:21:26 -0300

    feat: add project interface

---
+++ src/app/shared/interfaces/project.interface.ts
@@ -0,0 +1,53 @@
+//Ver se precisa exportar
+export interface GetSort {
+  empty: boolean;
+  unsorted: boolean;
+  sorted: boolean;
+}
+
+export interface IProjectGet {
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  content: IProject[];
+  number: number;
+  sort: GetSort;
+  first: boolean;
+  last: boolean;
+  numberOfElements: number;
+  pageable: {
+    offset: number;
+    sort: GetSort;
+    unpaged: boolean;
+    paged: boolean;
+    pageSize: number;
+    pageNumber: number;
+  };
+  empty: boolean;
+}
+
+export interface IProject {
+  id: number;
+  idStatus: number;
+  sigla: string;
+  titulo: string;
+  idEntidade: number;
+  valorEstimado: number;
+  idMicrorregioes: number[];
+  objetivo: string;
+  objetivoEspecifico: string;
+  situacaoProblema: string;
+  solucoesPropostas: string;
+  impactos: string;
+  arranjosInstitucionais: string;
+}
+
+export interface ProjectGetRequestBody {
+  page?: number;
+  size?: number;
+  sort?: string | Array<string>[];
+}
+
+export type ProjectCreate = Omit<IProject, 'id' | 'idStatus'>;
+
+export type ProjectEdit = Partial<IProject>;
