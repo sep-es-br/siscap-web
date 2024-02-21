0bb6f8107195b6dc806428cb76bf23976b6dffdb
Artur Faria <artur.faria.1992@gmail.com>
2024-02-21 11:35:19 -0300

    ref: add acessoCidadaoSignIn method

--- src/app/shared/services/authentication/authentication.service.ts
+++ src/app/shared/services/authentication/authentication.service.ts
@@ -1,31 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  private api = 'http://localhost:8080/siscap';
+  private _url = `${environment.api}/signin`;
 
-  constructor(private router: Router) {}
+  // private headers = {
+  //   headers: { 'Access-Control-Allow-Origin': 'http://localhost:8080' },
+  // };
+
+  constructor(private _http: HttpClient, private router: Router) {}
 
   public mockSignIn() {
-    this.router.navigateByUrl('/home');
+    this.router.navigateByUrl('/main/home');
   }
 
-  public signIn() {
-    window.location.href = this.getUrlForAuth();
+  public acessoCidadaoSignIn(): Observable<any> {
+    // return this._http.get(this._url, this.headers);
+    return this._http.get(this._url);
   }
 
-  private getUrlForAuth() {
-    return `${
-      this.api
-    }/oauth2/authorization/idsvr?front_callback_url=${this.getFrontFallbackUrl()}`;
-  }
+  // public signIn() {
+  //   window.location.href = this.getUrlForAuth();
+  // }
 
-  private getFrontFallbackUrl() {
-    const { protocol, host } = window.location;
-    // console.log(`${protocol}//${host}`);
-    return `${protocol}//${host}`;
-  }
+  // private getUrlForAuth() {
+  //   return `${
+  //     this.api
+  //   }/oauth2/authorization/idsvr?front_callback_url=${this.getFrontFallbackUrl()}`;
+  // }
+
+  // private getFrontFallbackUrl() {
+  //   const { protocol, host } = window.location;
+  //   // console.log(`${protocol}//${host}`);
+  //   return `${protocol}//${host}`;
+  // }
 }
