768b4d840da38beb42f8fecceb73d8659f73ea2e
Artur Faria <artur.faria.1992@gmail.com>
2024-02-22 17:26:26 -0300

    feat: add Acesso Cidadao auth token handling logic

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: false,
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  constructor(private _router: Router, private _route: ActivatedRoute) {
+    const token = atob(
+      this._router
+        .getCurrentNavigation()
+        ?.initialUrl.queryParamMap.get('token') as string
+    );
 
+    localStorage.setItem('token', token);
+  }
+
+  ngOnInit(): void {
+    this._router.navigate(['home'], { relativeTo: this._route });
+  }
 }
