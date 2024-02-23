ecc43aeeee0596f2dfc6126200c9847787e22c73
Artur Faria <artur.faria.1992@gmail.com>
2024-02-23 17:53:21 -0300

    ref: change breadcrumb component behavior (WiP)

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EventType, NavigationEnd, Router } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -6,4 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.css',
 })
-export class BreadcrumbComponent {}
+export class BreadcrumbComponent {
+  private breadcrumbNav: Array<string> = [];
+
+  constructor(private _router: Router) {
+    this._router.events
+      .pipe(
+        filter((event) => {
+          return event.type == EventType.NavigationEnd;
+        })
+      )
+      .subscribe(() => {
+        const currentNav = this._router
+          .getCurrentNavigation()
+          ?.extractedUrl.root.children['primary'].segments.filter((segment) => {
+            return segment.path != 'main' && segment.path != 'home';
+          });
+        currentNav?.forEach((segment) => !this.breadcrumbNav.includes(segment.path) ? this.breadcrumbNav.push(segment.path)  : null);
+        console.log(this.breadcrumbNav);
+        // console.log(currentNav);
+      });
+  }
+
+  navigateHome() {
+    // this._router.navigate([`${route}`], { relativeTo: this._route });
+    this._router.navigateByUrl('/main/home');
+  }
+}
