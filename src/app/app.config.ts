74133f21d2f2d9b46499b58bac1d2354980e9e47
Artur Faria <artur.faria.1992@gmail.com>
2024-02-23 14:12:28 -0300

    feat: add interceptor provider for HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+
+import { authInterceptor } from './shared/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
