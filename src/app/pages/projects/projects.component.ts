9f7ac57e5019935b6fd34a64a5495abd1d0f557e
Artur Faria <artur.faria.1992@gmail.com>
2024-02-28 17:33:00 -0300

    ref: change getProjetos subscribe

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -5,6 +5,9 @@ import { Subscription, first } from 'rxjs';
 
 import { ProjetosService } from '../../shared/services/projetos/projetos.service';
 import { IProject } from '../../shared/interfaces/project.interface';
+import { projetosColumnTitles } from '../../shared/utils/columnTitles.map';
+import { DataTableObject } from '../../shared/interfaces/dataTableObject.interface';
+// import { projetosColumnTitles } from '../../shared/utils/columnTitles.enum';
 
 @Component({
   selector: 'app-projects',
@@ -17,6 +20,8 @@ export class ProjectsComponent implements OnDestroy {
 
   public projetosList: IProject[] = [];
 
+  public projetosDataTableObject!: DataTableObject<IProject>;
+
   constructor(
     private _router: Router,
     private _route: ActivatedRoute,
@@ -25,8 +30,17 @@ export class ProjectsComponent implements OnDestroy {
     this._projetos$ = this._projetosService
       .getProjetos()
       .pipe(first())
+      //Substituir por Observer
       .subscribe((response) => {
         this.projetosList = response.content;
+      },
+      (error) => {},
+      () => {
+        this.projetosDataTableObject = {
+          dataArray: this.projetosList,
+          columnTitles: 'projetos',
+          omittedFields: ['id', 'idStatus']
+        }
       });
   }
 
@@ -43,45 +57,45 @@ export class ProjectsComponent implements OnDestroy {
 
   queryProject() {}
 
-  handleEvent(event: any) {
-    switch (event.type) {
-      case 'showObjectDetails':
-        //showDetails
-        console.log('show');
-        console.log(event.content);
-        break;
-      case 'editObject':
-        //editObject
-        this.redirectProjectCreateEdit(event.content.id);
-        break;
-      case 'deleteObject':
-        //deleteObject
-        if (
-          confirm(`
-        Tem certeza que deseja deletar o projeto?
-        Titulo: ${event.content.titulo}
-        `)
-        ) {
-          //Verificar erro do console
-          // console.log(this._route)
-          this._projetos$ = this._projetosService
-            .deleteProjeto(event.content.id)
-            .subscribe((response) => {
-              console.log(response);
-              if (response) {
-                alert('Projeto excluido com sucesso.');
-                //TODO: entender pq a pagina não recarrega
-                // this._router.navigate(['main', 'projects'], {
-                //   onSameUrlNavigation: 'reload',
-                // });
-              }
-            });
-        }
-        break;
-      default:
-        break;
-    }
-  }
+  // handleEvent(event: any) {
+  //   switch (event.type) {
+  //     case 'showObjectDetails':
+  //       //showDetails
+  //       console.log('show');
+  //       console.log(event.content);
+  //       break;
+  //     case 'editObject':
+  //       //editObject
+  //       this.redirectProjectCreateEdit(event.content.id);
+  //       break;
+  //     case 'deleteObject':
+  //       //deleteObject
+  //       if (
+  //         confirm(`
+  //       Tem certeza que deseja deletar o projeto?
+  //       Titulo: ${event.content.titulo}
+  //       `)
+  //       ) {
+  //         //Verificar erro do console
+  //         // console.log(this._route)
+  //         this._projetos$ = this._projetosService
+  //           .deleteProjeto(event.content.id)
+  //           .subscribe((response) => {
+  //             console.log(response);
+  //             if (response) {
+  //               alert('Projeto excluido com sucesso.');
+  //               //TODO: entender pq a pagina não recarrega
+  //               // this._router.navigate(['main', 'projects'], {
+  //               //   onSameUrlNavigation: 'reload',
+  //               // });
+  //             }
+  //           });
+  //       }
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
 
   ngOnDestroy(): void {
     this._projetos$.unsubscribe();
