a600928ac7008e6320446f676b4c08c366fb2adf
Artur Faria <artur.faria.1992@gmail.com>
2024-02-28 17:33:13 -0300

    feat: add columnTitles map

---
+++ src/app/shared/utils/columnTitles.map.ts
@@ -0,0 +1,19 @@
+export const projetosColumnTitles = {
+  sigla: 'Sigla',
+  titulo: 'Titulo',
+  valorEstimado: 'Valor',
+  objetivo: 'Objetivo',
+  objetivoEspecifico: 'Obj. Esp.',
+  idStatus: 'Status', // sujeito á alteração
+  idEntidade: 'Órgão',
+  situacaoProblema: 'Situação',
+  solucoesPropostas: 'Soluções',
+  impactos: 'Impactos',
+  arranjosInstitucionais: 'Arranjos',
+  idMicrorregioes: 'Microrregiões',
+} as Object;
+
+export const programasColumnTitles = {
+  prop1: 'Propriedade 1',
+  // replicar lógica para demais propriedades
+} as Object;
