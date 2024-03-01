cbc355a54684de4bb9f343125089bdf3a94e284b
Artur Faria <artur.faria.1992@gmail.com>
2024-03-01 14:41:37 -0300

    ref: add datatable componente declaration and export

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,12 +4,11 @@ import { NavMenuComponent } from './navmenu/navmenu.component';
 import { HeaderComponent } from './header/header.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { AlertComponent } from './alert/alert.component';
-import { DataTableComponent } from './data-table/data-table.component';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { LoadingComponent } from './loading/loading.component';
 import { BreadcrumbnavlinkPipe } from '../pipes/breadcrumbnavlink.pipe';
 import { DataTablesModule } from 'angular-datatables';
-import { NewDataTableComponent } from './new-data-table/new-data-table.component';
+import { DatatableComponent } from './datatable/datatable.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +16,7 @@ import { NewDataTableComponent } from './new-data-table/new-data-table.component
     HeaderComponent,
     BreadcrumbComponent,
     AlertComponent,
-    DataTableComponent,
-    NewDataTableComponent,
+    DatatableComponent,
     LoadingComponent,
   ],
   imports: [
@@ -32,8 +30,7 @@ import { NewDataTableComponent } from './new-data-table/new-data-table.component
     HeaderComponent,
     BreadcrumbComponent,
     AlertComponent,
-    DataTableComponent,
-    NewDataTableComponent,
+    DatatableComponent,
     LoadingComponent,
   ],
   providers: [CurrencyPipe, DatePipe],
