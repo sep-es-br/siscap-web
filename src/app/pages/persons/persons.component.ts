da7d0eec23cbb61a5fa54e7103da7346b6ff85f1
Artur Faria <artur.faria.1992@gmail.com>
2024-03-05 16:50:00 -0300

    feat: add persons component

---
+++ src/app/pages/persons/persons.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persons',
+  standalone: false,
+  templateUrl: './persons.component.html',
+  styleUrl: './persons.component.css',
+})
+export class PersonsComponent {}
