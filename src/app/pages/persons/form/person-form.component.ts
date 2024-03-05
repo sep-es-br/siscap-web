df6be285d5ca2dbb11ffcb04cf88b81bfd2e0451
Artur Faria <artur.faria.1992@gmail.com>
2024-03-05 16:50:12 -0300

    feat: add personform component

---
+++ src/app/pages/persons/form/person-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-form',
+  standalone: false,
+  templateUrl: './person-form.component.html',
+  styleUrl: './person-form.component.css',
+})
+export class PersonFormComponent {}
