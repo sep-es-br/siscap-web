8f55f2f26614aba7a70d2d87e6d879c03ce5a3b0
Artur Faria <artur.faria.1992@gmail.com>
2024-03-05 16:15:21 -0300

    perf: replace swtich clause for single return

--- src/app/pipes/breadcrumbnavlink.pipe.ts
+++ src/app/pipes/breadcrumbnavlink.pipe.ts
@@ -1,11 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-const enum breadCrumbTitles {
+enum breadCrumbTitles {
   main = 'Home',
-  projects = 'Projetos',
-  projectscreate = 'Novo Projeto',
-  projectsedit = 'Editar Projeto',
-  projectsdetails = 'Visualizar Projeto',
+
+  projetos = 'Projetos',
+  
+  projetoscriar = 'Novo Projeto',
+  projetoseditar = 'Editar Projeto',
+  projetosdetalhes = 'Visualizar Projeto',
 }
 
 @Pipe({
@@ -13,40 +15,7 @@ const enum breadCrumbTitles {
   standalone: true,
 })
 export class BreadcrumbnavlinkPipe implements PipeTransform {
-  transform(value: string, ...args: unknown[]): unknown {
-    let transformedValue = '';
-
-    switch (value) {
-      case 'main':
-        transformedValue = breadCrumbTitles.main;
-        break;
-
-      case 'projects':
-        transformedValue = breadCrumbTitles.projects;
-        break;
-
-      case 'projectscreate':
-        transformedValue = breadCrumbTitles.projectscreate;
-        break;
-
-      case 'projectsedit':
-        transformedValue = breadCrumbTitles.projectsedit;
-        break;
-
-      case 'projectsdetails':
-        transformedValue = breadCrumbTitles.projectsdetails;
-        break;
-        
-
-      // Inserir demais casos:
-      // case 'programs':
-      //  transformedValue = 'Programas';
-      //  break;
-
-      default:
-        break;
-    }
-
-    return transformedValue;
+  transform(value: string, ...args: unknown[]): string {
+    return breadCrumbTitles[value as keyof typeof breadCrumbTitles];
   }
 }
