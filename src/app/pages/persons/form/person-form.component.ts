acb72e7ac5f85122a33b8367cb44d338a1d69023
Artur Faria <artur.faria.1992@gmail.com>
2024-03-06 16:33:31 -0300

    ref: add placeholder parameters; test methods

--- src/app/pages/persons/form/person-form.component.ts
+++ src/app/pages/persons/form/person-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-person-form',
@@ -6,4 +6,28 @@ import { Component } from '@angular/core';
   templateUrl: './person-form.component.html',
   styleUrl: './person-form.component.css',
 })
-export class PersonFormComponent {}
+export class PersonFormComponent implements OnInit {
+  public loading: boolean = false;
+
+  public uploadedPhoto!: any;
+
+  public placeholderList: Array<{ id: number; label: string }> = [];
+
+  constructor() {}
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //     console.log(changes)
+  // }
+
+  // Not allowed to load local resource: file:///C:/fakepath/eu.jpeg
+
+  ngOnInit(): void {
+    console.log(this.uploadedPhoto);
+    console.log(typeof this.uploadedPhoto);
+  }
+
+  test() {
+    console.log(this.uploadedPhoto);
+    console.log(typeof this.uploadedPhoto);
+  }
+}
