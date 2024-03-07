ad8904e326c8f2417f5e2223fda2abf06d9895a2
Artur Faria <artur.faria.1992@gmail.com>
2024-03-07 09:49:27 -0300

    feat: create http-get interface

---
+++ src/app/shared/interfaces/http-get.interface.ts
@@ -0,0 +1,32 @@
+interface GetSort {
+  empty: boolean;
+  unsorted: boolean;
+  sorted: boolean;
+}
+
+export interface IHttpGetRequestBody {
+  page?: number;
+  size?: number;
+  sort?: string | Array<string>[];
+}
+
+export interface IHttpGetResponseBody<T> {
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  content: Array<T>;
+  number: number;
+  sort: GetSort;
+  first: boolean;
+  last: boolean;
+  numberOfElements: number;
+  pageable: {
+    offset: number;
+    sort: GetSort;
+    unpaged: boolean;
+    paged: boolean;
+    pageSize: number;
+    pageNumber: number;
+  };
+  empty: boolean;
+}
