5f2a32165ff5981afd03ab0a451e2299fa3c9add
Artur Faria <artur.faria.1992@gmail.com>
2024-03-12 17:48:55 -0300

    ref: remove datatable import

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,7 +4,7 @@ import { NavMenuComponent } from './navmenu/navmenu.component';
 import { HeaderComponent } from './header/header.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { AlertComponent } from './alert/alert.component';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+// import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { LoadingComponent } from './loading/loading.component';
 import { BreadcrumbnavlinkPipe } from '../pipes/breadcrumbnavlink.pipe';
 
@@ -18,7 +18,7 @@ import { BreadcrumbnavlinkPipe } from '../pipes/breadcrumbnavlink.pipe';
   ],
   imports: [
     CommonModule,
-    NgxDatatableModule,
+    // NgxDatatableModule,
     BreadcrumbnavlinkPipe,
   ],
   exports: [
