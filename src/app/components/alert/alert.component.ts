8266687206bd2eaf9879f31a6965d76b1598229c
Artur Faria <artur.faria.1992@gmail.com>
2024-03-12 17:49:04 -0300

    ref: change alert component

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-alert',
@@ -6,4 +13,28 @@ import { Component } from '@angular/core';
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.css',
 })
-export class AlertComponent {}
+export class AlertComponent {
+  @Input('control') control!: AbstractControl<any, any>;
+
+  constructor() {}
+
+  formControlErrorMap(control: AbstractControl): string {
+    if (control.errors) {
+      return Object.keys(control.errors)
+        .map((controlError) => {
+          switch (controlError) {
+            case 'required':
+              return 'Campo obrigatório';
+            case 'email':
+              return 'Email inválido';
+
+            default:
+              return '';
+          }
+        })
+        .join(', ');
+    }
+
+    return '';
+  }
+}
