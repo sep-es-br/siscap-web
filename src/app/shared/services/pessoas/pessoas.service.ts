5193c3cd7212d480ab712a012ab74a09cb632fd0
Artur Faria <artur.faria.1992@gmail.com>
2024-03-13 18:35:37 -0300

    ref: change putPessoas body type to FormData; remove excess methods

--- src/app/shared/services/pessoas/pessoas.service.ts
+++ src/app/shared/services/pessoas/pessoas.service.ts
@@ -16,7 +16,6 @@ import {
 })
 export class PessoasService {
   private _url = `${environment.apiUrl}/pessoas`;
-  // private _url = `${environment.apiUrl}/pessoasaa`;
 
   constructor(private _http: HttpClient) {}
 
@@ -24,7 +23,7 @@ export class PessoasService {
     return this._http.get<IPerson>(`${this._url}/${id}`);
   }
 
-  putPessoa(id: number, body: IPersonEdit): Observable<IPerson> {
+  putPessoa(id: number, body: FormData): Observable<IPerson> {
     return this._http.put<IPerson>(`${this._url}/${id}`, body);
   }
 
@@ -37,31 +36,7 @@ export class PessoasService {
     return this._http.get<IPersonGet>(this._url);
   }
 
-  // Formato de envio não é JSON -> headers: Content-Type: multipart/formdata
   postPessoa(body: FormData): Observable<IPerson> {
     return this._http.post<IPerson>(this._url, body);
   }
-
-  // getPessoaImagemPerfil(id: number): Observable<Blob> {
-  //   const headers = new HttpHeaders({
-  //     Accept: 'image/*',
-  //   });
-  //   return this._http.get(`${this._url}/${id}/imagem-perfil`, {
-  //     headers: headers,
-  //     responseType: 'blob',
-  //   });
-  // }
 }
-
-/*
-
-Ideia select pais/estado/cidade
-
-getCidades(filtro: string, id:numnber){
-  //codigo
-}
-
-this.getCidades('PAIS', 5) -> retorna cidades de acordo com o pais
-this.getCidades('ESTADO', 9) -> retorna cidades de acord com o estado
-
-*/
