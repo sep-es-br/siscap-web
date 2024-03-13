7d73344ed3bd4a3fb5a960e6418d935f591fa504
Artur Faria <artur.faria.1992@gmail.com>
2024-03-13 18:32:31 -0300

    perf: refactor component

--- src/app/pages/projects/form/project-form.component.ts
+++ src/app/pages/projects/form/project-form.component.ts
@@ -1,23 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { Subscription, finalize, map, tap } from 'rxjs';
+import { Observable, finalize, first, tap } from 'rxjs';
 
 import * as _ from 'lodash';
 
-import { projectCreateModel } from '../../../shared/models/projectCreate.model';
-import { FormFactoryService } from '../../../shared/services/form-factory/form-factory.service';
-import { MicrorregioesService } from '../../../shared/services/microrregioes/microrregioes.service';
-import { IMicrorregiao } from '../../../shared/interfaces/microrregiao.interface';
-import { EntidadesService } from '../../../shared/services/entidades/entidades.service';
-import { IEntidade } from '../../../shared/interfaces/entidade.interface';
-import { ProjetosService } from '../../../shared/services/projetos/projetos.service';
 import {
   IProject,
   IProjectCreate,
   IProjectEdit,
 } from '../../../shared/interfaces/project.interface';
+import { ISelectList } from '../../../shared/interfaces/select-list.interface';
+import { ProjetosService } from '../../../shared/services/projetos/projetos.service';
+import { SelectListService } from '../../../shared/services/select-list/select-list.service';
 
 @Component({
   selector: 'app-create',
@@ -25,180 +21,158 @@ import {
   templateUrl: './project-form.component.html',
   styleUrl: './project-form.component.css',
 })
-export class ProjectFormComponent implements OnInit, OnDestroy {
+export class ProjectFormComponent implements OnInit {
   public projectForm!: FormGroup;
 
   public loading: boolean = false;
 
   public formMode!: string;
 
+  private _prepareForm$!: Observable<IProject>;
   public projectEditId!: number;
   public projectFormInitialValue!: IProjectCreate;
 
-  private _microrregioes$!: Subscription;
-  private _entidades$!: Subscription;
-
-  public microrregioesList: IMicrorregiao[] = [];
-  public entidadesList: IEntidade[] = [];
-
-  public projectCreateObject: projectCreateModel = {
-    sigla: {
-      initialValue: '',
-      validators: {
-        required: true,
-        maxLength: 12,
-      },
-    },
-    titulo: {
-      initialValue: '',
-      validators: {
-        required: true,
-        maxLength: 150,
-      },
-    },
-    idEntidade: {
-      initialValue: null,
-      validators: {
-        required: true,
-        min: 1,
-      },
-    },
-    valorEstimado: {
-      initialValue: null,
-      validators: {
-        required: true,
-        min: 1,
-      },
-    },
-    idMicrorregioes: {
-      initialValue: [],
-      validators: {
-        required: true,
-      },
-    },
-    objetivo: {
-      initialValue: '',
-      validators: {
-        required: true,
-        maxLength: 2000,
-      },
-    },
-    objetivoEspecifico: {
-      initialValue: '',
-      validators: {
-        required: true,
-        maxLength: 2000,
-      },
-    },
-    situacaoProblema: {
-      initialValue: '',
-      validators: {
-        required: true,
-        maxLength: 2000,
-      },
-    },
-    solucoesPropostas: {
-      initialValue: '',
-      validators: {
-        required: true,
-        maxLength: 2000,
-      },
-    },
-    impactos: {
-      initialValue: '',
-      validators: {
-        required: true,
-        maxLength: 2000,
-      },
-    },
-    arranjosInstitucionais: {
-      initialValue: '',
-      validators: {
-        required: true,
-        maxLength: 2000,
-      },
-    },
-  };
+  public microrregioesList: ISelectList[] = [];
+  public entidadesList: ISelectList[] = [];
+  public planosList: ISelectList[] = [];
+  public pessoasList: ISelectList[] = [];
 
   constructor(
-    private _formFactory: FormFactoryService,
-    private _microrregioesService: MicrorregioesService,
-    private _entidadesService: EntidadesService,
+    private _fb: FormBuilder,
     private _projetosService: ProjetosService,
+    private _selectListService: SelectListService,
     private _route: ActivatedRoute,
     private _router: Router
   ) {
     this.formMode = this._route.snapshot.params['mode'];
     this.projectEditId = this._route.snapshot.queryParams['id'] ?? null;
 
-    this._microrregioes$ = this._microrregioesService
+    this._prepareForm$ = this._projetosService
+      .getProjetoById(this.projectEditId)
+      .pipe(
+        first(),
+        tap((response) => {
+          this.initForm(response);
+        }),
+        finalize(() => {
+          this.projectFormInitialValue = this.projectForm.value;
+
+          if (this.formMode == 'detalhes') {
+            const controls = this.projectForm.controls;
+            for (const key in controls) {
+              controls[key].disable();
+            }
+          }
+
+          this.loading = false;
+        })
+      );
+
+    this._selectListService
       .getMicrorregioes()
       .pipe(
-        tap((value) => {
-          this.microrregioesList = value;
+        first(),
+        tap((response) => {
+          this.microrregioesList = response;
         })
       )
       .subscribe();
 
-    this._entidades$ = this._entidadesService
+    this._selectListService
       .getEntidades()
       .pipe(
-        tap((value) => {
-          this.entidadesList = value;
+        first(),
+        tap((response) => {
+          this.entidadesList = response;
+        })
+      )
+      .subscribe();
+
+    this._selectListService
+      .getPlanos()
+      .pipe(
+        first(),
+        tap((response) => {
+          this.planosList = response;
+        })
+      )
+      .subscribe();
+
+    this._selectListService
+      .getPessoas()
+      .pipe(
+        first(),
+        tap((response) => {
+          this.pessoasList = response;
         })
       )
       .subscribe();
   }
 
   ngOnInit(): void {
-    this.initForm();
-
-    if (this.formMode == 'editar' || this.formMode == 'detalhes') {
-      this.loading = true;
-
-      // Preenche os controles com os valores do projeto selecionado
-      this._projetosService
-        .getProjetoById(this.projectEditId)
-        .pipe(
-          map<IProject, IProjectCreate>((project) => {
-            return _.pick(
-              project,
-              _.keys(this.projectForm.controls)
-            ) as IProjectCreate;
-          }),
-          tap((project) => {
-            _.forIn(project, (value, key) => {
-              this.projectForm.controls[key]?.setValue(value);
-            });
-          }),
-          finalize(() => {
-            this.loading = false;
-            this.projectFormInitialValue = this.projectForm.value;
-          })
-        )
-        .subscribe((next) => {
-          this.projectForm.valueChanges.subscribe((change) => {
-            this.projectForm.setErrors(
-              _.isEqual(next, change) ? { identical: true } : null
-            );
-          });
-        });
-
-      if (this.formMode == 'detalhes') {
-        const controls = this.projectForm.controls;
-        for (const key in controls) {
-          controls[key].disable();
-        }
-      }
+    if (this.formMode == 'criar') {
+      this.initForm();
+      return;
     }
+
+    this.loading = true;
+
+    this._prepareForm$.subscribe();
   }
 
   /**
    * Método para inicialização do formulário.
    *
    */
-  initForm() {
-    this.projectForm = this._formFactory.generateForm(this.projectCreateObject);
+  initForm(project?: IProject) {
+    const nnfb = this._fb.nonNullable;
+    this.projectForm = nnfb.group({
+      sigla: nnfb.control(project?.sigla ?? '', {
+        validators: [Validators.required, Validators.maxLength(12)],
+      }),
+      titulo: nnfb.control(project?.titulo ?? '', {
+        validators: [Validators.required, Validators.maxLength(150)],
+      }),
+      idEntidade: nnfb.control(project?.idEntidade ?? null, {
+        validators: Validators.required,
+      }),
+      valorEstimado: nnfb.control(project?.valorEstimado ?? null, {
+        validators: [Validators.required, Validators.min(1)],
+      }),
+      idMicrorregioes: nnfb.control(project?.idMicrorregioes ?? [], {
+        validators: Validators.required,
+      }),
+      objetivo: nnfb.control(project?.objetivo ?? '', {
+        validators: [Validators.required, Validators.maxLength(2000)],
+      }),
+      objetivoEspecifico: nnfb.control(project?.objetivoEspecifico ?? '', {
+        validators: [Validators.required, Validators.maxLength(2000)],
+      }),
+      situacaoProblema: nnfb.control(project?.situacaoProblema ?? '', {
+        validators: [Validators.required, Validators.maxLength(2000)],
+      }),
+      solucoesPropostas: nnfb.control(project?.solucoesPropostas ?? '', {
+        validators: [Validators.required, Validators.maxLength(2000)],
+      }),
+      impactos: nnfb.control(project?.impactos ?? '', {
+        validators: [Validators.required, Validators.maxLength(2000)],
+      }),
+      arranjosInstitucionais: nnfb.control(
+        project?.arranjosInstitucionais ?? '',
+        {
+          validators: [Validators.required, Validators.maxLength(2000)],
+        }
+      ),
+      idPessoasEquipeElab: nnfb.control(project?.idPessoasEquipeElab ?? [], {
+        validators: Validators.required,
+      }),
+      idPlano: nnfb.control(project?.idPlano ?? [], {
+        validators: Validators.required,
+      }),
+      //AInda não implementados
+      eixo: nnfb.control({ value: null, disabled: true }),
+      area: nnfb.control({ value: null, disabled: true }),
+    });
   }
 
   /**
@@ -218,8 +192,12 @@ export class ProjectFormComponent implements OnInit, OnDestroy {
    *
    */
   submitProjectForm(form: FormGroup) {
+    for (const key in form.controls) {
+      form.controls[key].markAsTouched();
+    }
+
     if (form.invalid) {
-      alert('Formulário contém erros. Por favor verificar os campos.');
+      // alert('Formulário contém erros. Por favor verificar os campos.');
       return;
     }
 
@@ -240,15 +218,10 @@ export class ProjectFormComponent implements OnInit, OnDestroy {
         break;
 
       case 'editar':
-        // Retorna um objeto que contém somente os valores alterados.
-        const editPayload = _.pickBy(form.value, (value, key) => {
-          return (
-            value !=
-            this.projectFormInitialValue[
-              key as keyof typeof this.projectFormInitialValue
-            ]
-          );
-        }) as IProjectEdit;
+        const editPayload = this.prepareEditForm(
+          form.value,
+          this.projectFormInitialValue
+        ) as IProjectEdit;
 
         this._projetosService
           .putProjeto(this.projectEditId, editPayload)
@@ -266,8 +239,9 @@ export class ProjectFormComponent implements OnInit, OnDestroy {
     }
   }
 
-  ngOnDestroy(): void {
-    this._microrregioes$.unsubscribe();
-    this._entidades$.unsubscribe();
+  prepareEditForm(formCurrentValue: any, formInitialValue: any) {
+    return _.pickBy(formCurrentValue, (value, key) => {
+      return value != formInitialValue[key];
+    });
   }
 }
