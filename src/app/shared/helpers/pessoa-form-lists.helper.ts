0fd360174393f3b588d73e113f7d5df390265fbe
Artur Faria <artur.faria.1992@gmail.com>
2024-03-13 18:32:42 -0300

    ref: change static lists data type

--- src/app/shared/helpers/pessoa-form-lists.helper.ts
+++ src/app/shared/helpers/pessoa-form-lists.helper.ts
@@ -1,12 +1,12 @@
 export abstract class PessoaFormLists {
-  static nacionalidadesList = [{ id: 1, label: 'Brasileira' }];
+  static nacionalidadesList = ['Brasileira'];
 
   static generosList = [
-    { id: 1, label: 'Masculino' },
-    { id: 2, label: 'Feminino' },
-    { id: 3, label: 'Trans Masculino' },
-    { id: 4, label: 'Trans Feminino' },
-    { id: 5, label: 'Não Binário' },
-    { id: 6, label: 'Outro' },
+    'Masculino',
+    'Feminino',
+    'Trans Masculino',
+    'Trans Feminino',
+    'Não Binário',
+    'Outro',
   ];
 }
