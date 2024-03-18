0893ea81d7eea66c699828f925f931ba8f79dd76
Artur Faria <artur.faria.1992@gmail.com>
2024-03-18 09:48:19 -0300

    ref: add table

--- src/app/pages/entities/entities.component.ts
+++ src/app/pages/entities/entities.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { EntidadesService } from '../../shared/services/entidades/entidades.service';
+
 import { first } from 'rxjs';
+
+import { EntidadesService } from '../../shared/services/entidades/entidades.service';
+import { IEntityTable } from '../../shared/interfaces/entity.interface';
 
 @Component({
   selector: 'app-entities',
@@ -10,70 +13,79 @@ import { first } from 'rxjs';
   styleUrl: './entities.component.css',
 })
 export class EntitiesComponent {
-  // public entidadesList: Array<any> = [];
-  // constructor(
-  //   private _router: Router,
-  //   private _route: ActivatedRoute,
-  //   private _entidadesService: EntidadesService
-  // ) {
-  //   this._entidadesService
-  //     .getEntidades()
-  //     .pipe(first())
-  //     .subscribe((response) => {
-  //       this.entidadesList = response.content;
-  //     });
-  // }
-  // queryEntity() {}
-  // entityDetails(data: any) {
-  //   this._router.navigate(['form', 'detalhes'], {
-  //     relativeTo: this._route,
-  //     queryParams: { id: data.id },
-  //   });
-  // }
-  // entityEdit(data: any) {
-  //   this._router.navigate(['form', 'editar'], {
-  //     relativeTo: this._route,
-  //     queryParams: { id: data.id },
-  //   });
-  // }
-  // entityDelete(data: any) {
-  //   if (
-  //     // Trocar texto de 'usuario' para 'entidade'
-  //     confirm(`
-  //           Tem certeza que deseja deletar o usuário?
-  //           Nome: ${data.nome}
-  //           E-mail: ${data.email}
-  //           `)
-  //   ) {
-  //     this._entidadesService.deleteEntidade(data.id).subscribe(
-  //       (response) => {
-  //         console.log(response);
-  //         if (response) {
-  //           alert('Usuário excluido com sucesso.');
-  //         }
-  //       },
-  //       (err) => {},
-  //       () => {
-  //         this._router
-  //           .navigateByUrl('/', { skipLocationChange: true })
-  //           .then(() => this._router.navigate(['main', 'pessoas']));
-  //       }
-  //     );
-  //   }
-  // }
-  // actionEvent(type: string, data: any) {
-  //   switch (type) {
-  //     case 'detalhes':
-  //       this.entityDetails(data);
-  //       break;
-  //     case 'editar':
-  //       this.entityEdit(data);
-  //       break;
-  //     case 'deletar':
-  //       this.entityDelete(data);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
+  public entidadesList: Array<IEntityTable> = [];
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _entidadesService: EntidadesService
+  ) {
+    this._entidadesService
+      .getEntidades()
+      .pipe(first())
+      .subscribe((response) => {
+        this.entidadesList = response.content;
+      });
+  }
+
+  convertByteArraytoImg(data: ArrayBuffer): string {
+    return 'data:image/jpeg;base64,' + data;
+  }
+
+  queryEntity() {}
+
+  entityDetails(data: any) {
+    this._router.navigate(['form', 'detalhes'], {
+      relativeTo: this._route,
+      queryParams: { id: data.id },
+    });
+  }
+
+  entityEdit(data: any) {
+    this._router.navigate(['form', 'editar'], {
+      relativeTo: this._route,
+      queryParams: { id: data.id },
+    });
+  }
+
+  entityDelete(data: any) {
+    if (
+      confirm(`
+            Tem certeza que deseja deletar a entidade?
+            Nome: ${data.nome}
+            Sigla: ${data.abreviatura}
+            `)
+    ) {
+      this._entidadesService.deleteEntidade(data.id).subscribe(
+        (response) => {
+          console.log(response);
+          if (response) {
+            alert('Entidade excluída com sucesso.');
+          }
+        },
+        (err) => {},
+        () => {
+          this._router
+            .navigateByUrl('/', { skipLocationChange: true })
+            .then(() => this._router.navigate(['main', 'entidades']));
+        }
+      );
+    }
+  }
+
+  actionEvent(type: string, data: any) {
+    switch (type) {
+      case 'detalhes':
+        this.entityDetails(data);
+        break;
+      case 'editar':
+        this.entityEdit(data);
+        break;
+      case 'deletar':
+        this.entityDelete(data);
+        break;
+      default:
+        break;
+    }
+  }
 }
