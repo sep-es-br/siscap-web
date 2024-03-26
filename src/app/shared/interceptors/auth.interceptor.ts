ae5f8a112955d7bcb775bcf3c7157ef809601b4b
Artur Faria <artur.faria.1992@gmail.com>
2024-03-26 17:52:04 -0300

    ref: revert auth interceptor to previous state

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -1,11 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { catchError } from 'rxjs';
-import { SiscapErrorHandler } from '../handlers/error-handler';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const router = new Router()
-
   const reqClone = req.clone({
     headers: req.headers.set(
       'Authorization',
@@ -13,10 +8,5 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     ),
   });
 
-  return next(reqClone).pipe(
-    catchError((err) => {
-      new SiscapErrorHandler(err, router).handleError()
-      throw new Error('Ocorreu um erro ao processar a requisição. Erro: ' + err.status) 
-    })
-  );
+  return next(reqClone);
 };
