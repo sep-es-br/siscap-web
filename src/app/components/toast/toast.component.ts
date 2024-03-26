962c38c247fb7f84a498ca073fc86228b54fef84
Artur Faria <artur.faria.1992@gmail.com>
2024-03-26 17:50:52 -0300

    ref: change toast component

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
 
+import { ErrorHandlerService } from '../../shared/services/error-handler/error-handler.service';
+import { ToastErrorInfoMap } from '../../shared/utils/toast-error-info-map';
+
 @Component({
-  selector: 'app-toast',
+  selector: 'siscap-toast',
   standalone: false,
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.scss',
 })
-export class ToastComponent {}
+export class ToastComponent {
+  public displayToast: boolean = false;
+  public toastHeader: string = '';
+  public toastBody: string = '';
+
+  constructor(private _errorHandlerService: ErrorHandlerService) {
+    this._errorHandlerService.errorEmitter$.subscribe((value) => {
+      if (value) {
+        const toastErrorInfo = ToastErrorInfoMap[value];
+        this.toastHeader = toastErrorInfo.header;
+        this.toastBody = toastErrorInfo.body;
+        this.displayToast = true;
+      }
+    });
+  }
+
+  public resetToast() {
+    this.displayToast = false;
+    this.toastHeader = '';
+    this.toastBody = '';
+    this._errorHandlerService.errorEmitter$.next(undefined);
+  }
+}
