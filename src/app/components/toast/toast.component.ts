ba797173194d68324f15064ce67f4ec9064f9bb2
Artur Faria <artur.faria.1992@gmail.com>
2024-03-26 13:22:04 -0300

    feat: add toast component

---
+++ src/app/components/toast/toast.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast',
+  standalone: false,
+  templateUrl: './toast.component.html',
+  styleUrl: './toast.component.scss',
+})
+export class ToastComponent {}
