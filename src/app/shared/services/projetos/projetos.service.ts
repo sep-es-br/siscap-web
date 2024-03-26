35b28634b9ae474c2b43ece96caa6abee86328db
Artur Faria <artur.faria.1992@gmail.com>
2024-03-26 17:52:36 -0300

    feat: add error-handle service logic to http methods

--- src/app/shared/services/projetos/projetos.service.ts
+++ src/app/shared/services/projetos/projetos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 
 import { environment } from '../../../../environments/environment';
 import {
@@ -10,6 +10,7 @@ import {
   IProjectEdit,
   IProjectGet,
 } from '../../interfaces/project.interface';
+import { ErrorHandlerService } from '../error-handler/error-handler.service';
 
 @Injectable({
   providedIn: 'root',
@@ -17,26 +18,56 @@ import {
 export class ProjetosService {
   private _url = `${environment.apiUrl}/projetos`;
 
-  constructor(private _http: HttpClient) {}
+  constructor(
+    private _http: HttpClient,
+    private _errorHandlerService: ErrorHandlerService
+  ) {}
 
   getProjetoById(id: number): Observable<IProject> {
-    return this._http.get<IProject>(`${this._url}/${id}`);
+    return this._http.get<IProject>(`${this._url}/${id}`).pipe(
+      catchError((err: HttpErrorResponse) => {
+        this._errorHandlerService.handleError(err);
+        throw new Error('Ocorreu um erro ao processar a requisição.');
+      })
+    );
   }
 
   putProjeto(id: number, body: IProjectEdit): Observable<IProject> {
-    return this._http.put<IProject>(`${this._url}/${id}`, body);
+    return this._http.put<IProject>(`${this._url}/${id}`, body).pipe(
+      catchError((err: HttpErrorResponse) => {
+        this._errorHandlerService.handleError(err);
+        throw new Error('Ocorreu um erro ao processar a requisição.');
+      })
+    );
   }
 
   deleteProjeto(id: number): Observable<string> {
-    return this._http.delete(`${this._url}/${id}`, { responseType: 'text' });
+    return this._http
+      .delete(`${this._url}/${id}`, { responseType: 'text' })
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          this._errorHandlerService.handleError(err);
+          throw new Error('Ocorreu um erro ao processar a requisição.');
+        })
+      );
   }
 
   // TODO: Ver como anexar body no get (IHttpGetRequestBody)
   getProjetos(): Observable<IProjectGet> {
-    return this._http.get<IProjectGet>(this._url);
+    return this._http.get<IProjectGet>(this._url).pipe(
+      catchError((err: HttpErrorResponse) => {
+        this._errorHandlerService.handleError(err);
+        throw new Error('Ocorreu um erro ao processar a requisição.');
+      })
+    );
   }
 
   postProjeto(body: IProjectCreate): Observable<IProject> {
-    return this._http.post<IProject>(this._url, body);
+    return this._http.post<IProject>(this._url, body).pipe(
+      catchError((err: HttpErrorResponse) => {
+        this._errorHandlerService.handleError(err);
+        throw new Error('Ocorreu um erro ao processar a requisição.');
+      })
+    );
   }
 }
