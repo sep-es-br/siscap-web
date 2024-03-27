13637e6951051bd77c1b280243b1e356a137f3bc
Artur Faria <artur.faria.1992@gmail.com>
2024-03-27 10:53:40 -0300

    docs: add documentation

--- src/app/shared/services/toast-notifier/toast-notifier.service.ts
+++ src/app/shared/services/toast-notifier/toast-notifier.service.ts
@@ -4,11 +4,16 @@ import { Subject } from 'rxjs';
 
 interface IToastNotification {
   type: string;
-  code?: number | undefined;
+  code?: number;
   source?: string;
   method?: string;
 }
 
+/**
+ * @service
+ * Serviço para emitir e receber notificações de locais diferentes da aplicação, com o propósito de alimentar `ToastComponent` e gerar um toast com as informações apropriadas.
+ *
+ */
 @Injectable({
   providedIn: 'root',
 })
@@ -18,7 +23,14 @@ export class ToastNotifierService {
 
   constructor() {}
 
-  public notifySuccess(source: string, method: string) {
+  /**
+   * @public
+   * Método que emite notificação de sucesso.
+   *
+   * @param {string} source - Local de origem da chamada. Utilizado para extrair informação de toast de `ToastSuccessInfoMap`.
+   * @param {string} method - Método da chamada. Utilizado para extrair informação de toast de `ToastSuccessInfoMap`.
+   */
+  public notifySuccess(source: string, method: string): void {
     this.toastNotifier$.next({
       type: 'success',
       source: source,
@@ -26,21 +38,40 @@ export class ToastNotifierService {
     });
   }
 
-  public notifyError(code: number | undefined) {
+  /**
+   * @public
+   * Método que emite notificação de erro.
+   *
+   * @param {number} code - Código de status do erro. Utilizado para extrair informação de toast de `ToastErrorInfoMap`.
+   */
+  public notifyError(code: number): void {
     this.toastNotifier$.next({ type: 'error', code: code });
   }
 
-  public clearNotice() {
+  /**
+   * @public
+   * Método que emite valor falsário com o propósito de limpar o toast.
+   */
+  public clearNotice(): void {
     this.toastNotifier$.next({ type: '' });
   }
 
+  /**
+   * @public
+   * Método para redirecionar o usuário após sucesso/erro de chamada do método.
+   *
+   * A lógica de redirecionamento foi centralizada neste serviço á fim de reaproveitamento.
+   *
+   * @param {Router} routerInstance - Instância de `Router` dentro do componente ou serviço pertinente.
+   * @param {string} url - A url á ser navegada.
+   * @param {boolean} samePage - Utilizado para adaptar lógica de navegação quando navegação é para a mesma página (caso `deleteProjeto`)
+   */
   public redirectOnToastClose(
     routerInstance: Router,
     url: string,
     samePage?: boolean
-  ) {
+  ): void {
     this.toastNotifier$.subscribe((notice) => {
-      console.log(!!samePage);
       if (!notice.type) {
         !!samePage
           ? routerInstance
