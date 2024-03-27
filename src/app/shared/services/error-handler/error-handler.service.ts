b1997328d10e7af6d60bbc811454143793cc53da
Artur Faria <artur.faria.1992@gmail.com>
2024-03-27 10:53:18 -0300

    docs: add documentation

--- src/app/shared/services/error-handler/error-handler.service.ts
+++ src/app/shared/services/error-handler/error-handler.service.ts
@@ -8,6 +8,10 @@ type ErrorHandleFn = {
   [statusCode: string]: Function;
 };
 
+/**
+ * @service
+ * Serviço para tratar erros de HTTP dentro do sistema. Recolhe erros e utiliza um serviço de notificação (`ToastNotifierService`) para oferecer feedback ao usuário.
+ */
 @Injectable({
   providedIn: 'root',
 })
@@ -23,8 +27,17 @@ export class ErrorHandlerService {
     private _toastNotifierService: ToastNotifierService,
     private _router: Router
   ) {}
-
-  public handleError(error: HttpErrorResponse) {
+  /**
+   * @public
+   * Método público invocado dentro dos métodos apropriados (ex: `getProjeto` dentro de `ProjetosService`).
+   *
+   * Itera por um mapa contendo métodos apropriados para cada tipo de erro, utilizando o código de status do erro (`error.status`).
+   * O retorno do mapa é uma função, que é chamada através de `Function.apply()` passando `this` como escopo da função de `[error]` como array de argumentos á serem substituídos.
+   * No caso desse serviço, apenas o erro original `error` é passado.
+   *
+   * @param {HttpErrorResponse} error - O erro fornecido pelo seletor do operador RxJS `catchError`.
+   */
+  public handleError(error: HttpErrorResponse): void {
     const errorHandleFunction =
       this._errorHandleFnMap[this._errorCodesList[error.status]] ??
       this.fallbackFuncion;
@@ -32,7 +45,15 @@ export class ErrorHandlerService {
     errorHandleFunction.apply(this, [error]);
   }
 
-  private handleUnauthorized(errorArg: HttpErrorResponse) {
+  /**
+   * @private
+   * Método que lida com o erro 401 - `Unauthorized`.
+   *
+   * Armazena o caminho local para redirecionar o usuário após ele refazer o login.
+   *
+   * @param {HttpErrorResponse} errorArg - Argumento local de erro, substituido pelo erro original da chamada `Function.apply()` de `handleError()`.
+   */
+  private handleUnauthorized(errorArg: HttpErrorResponse): void {
     this._toastNotifierService.notifyError(errorArg.status);
 
     localStorage.setItem('currentUrl', this._router.url);
@@ -40,13 +61,27 @@ export class ErrorHandlerService {
     this._toastNotifierService.redirectOnToastClose(this._router, 'login');
   }
 
-  private handleInternalServerError(errorArg: HttpErrorResponse) {
+  /**
+   * @private
+   * Método que lida com o erro 500 - `Internal Server Error`.
+   *
+   * Apenas notifica o usuário do erro através de um toast e o redireciona para a página principal.
+   *
+   * @param {HttpErrorResponse} errorArg - Argumento local de erro, substituido pelo erro original da chamada `Function.apply()` de `handleError()`.
+   */
+  private handleInternalServerError(errorArg: HttpErrorResponse): void {
     this._toastNotifierService.notifyError(errorArg.status);
 
     this._toastNotifierService.redirectOnToastClose(this._router, 'main');
   }
 
-  private fallbackFuncion(errorArg: HttpErrorResponse) {
+  /**
+   * @private
+   * Método de fallback caso o erro fornecido não seja contemplado em `errorHandleFnMap`.
+   *
+   * @param {HttpErrorResponse} errorArg - Argumento local de erro, substituido pelo erro original da chamada `Function.apply()` de `handleError()`.
+   */
+  private fallbackFuncion(errorArg: HttpErrorResponse): void {
     console.log(
       `Tratamento de erro para ${errorArg.status}: ${
         this._errorCodesList[errorArg.status]
