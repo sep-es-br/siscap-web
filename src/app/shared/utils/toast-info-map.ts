695ecef8e60a361f67f8eef7245dd5542f259084
Artur Faria <artur.faria.1992@gmail.com>
2024-03-27 14:36:11 -0300

    ref: change ToastErrorInfoMap 401 body value

--- src/app/shared/utils/toast-info-map.ts
+++ src/app/shared/utils/toast-info-map.ts
@@ -18,7 +18,7 @@ export const ToastErrorInfoMap: ToastErrorInfo = {
   },
   401: {
     header: 'Erro 401 - Não Autorizado',
-    body: 'A token de autenticação não é válida. O sistema será redirecionado para a página de login para tentar a autenticação novamente. Ao completar o processo, você retornará para a página que estava anteriormente.',
+    body: 'O token de autenticação não é válido. O sistema será redirecionado para a página de login para tentar a autenticação novamente. Ao completar o processo, você retornará para a página que estava anteriormente.',
   },
   500: {
     header: 'Erro 500 - Erro Interno de Servidor',
