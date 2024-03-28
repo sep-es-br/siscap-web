c06d432191f6c084b7edbcf3594da930cf85515b
Artur Faria <artur.faria.1992@gmail.com>
2024-03-28 16:15:39 -0300

    ref: change error-handler service

--- src/app/shared/services/error-handler/error-handler.service.ts
+++ src/app/shared/services/error-handler/error-handler.service.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 
-import { ToastNotifierService } from '../toast-notifier/toast-notifier.service';
+import { ToastService } from '../toast/toast.service';
+import { ToastErrorInfoMap } from '../../utils/toast-info-map';
 
-type ErrorHandleFn = {
-  [statusCode: string]: Function;
-};
+interface IHandleErrorOptions {
+  [status: string]: Function;
+}
 
 /**
  * @service
- * Serviço para tratar erros de HTTP dentro do sistema. Recolhe erros e utiliza um serviço de notificação (`ToastNotifierService`) para oferecer feedback ao usuário.
+ * Serviço para tratar erros de HTTP dentro do sistema. Recolhe erros e utiliza um serviço de toast (`ToastService`) para oferecer feedback ao usuário.
  */
 @Injectable({
   providedIn: 'root',
@@ -18,90 +19,72 @@ type ErrorHandleFn = {
 export class ErrorHandlerService {
   private _errorCodesList: typeof HttpStatusCode = HttpStatusCode;
 
-  private _errorHandleFnMap: ErrorHandleFn = {
-    Unauthorized: this.handleUnauthorized,
-    InternalServerError: this.handleInternalServerError,
-    BadRequest: this.handleBadRequest,
+  private _handleErrorOptions: IHandleErrorOptions = {
+    BadRequest: this.handleBadRequestOptions,
+    Unauthorized: this.handleUnauthorizedOptions,
+    InternalServerError: this.handleInternalServerErrorOptions,
   };
 
-  constructor(
-    private _toastNotifierService: ToastNotifierService,
-    private _router: Router
-  ) {}
+  constructor(private _toastService: ToastService, private _router: Router) {}
   /**
    * @public
-   * Método público invocado dentro dos métodos apropriados (ex: `getProjeto` dentro de `ProjetosService`).
+   * Método público invocado dentro dos métodos apropriados dos serviços (ex: `getProjeto` dentro de `ProjetosService`).
    *
-   * Itera por um mapa contendo métodos apropriados para cada tipo de erro, utilizando o código de status do erro (`error.status`).
-   * O retorno do mapa é uma função, que é chamada através de `Function.apply()` passando `this` como escopo da função de `[error]` como array de argumentos á serem substituídos.
-   * No caso desse serviço, apenas o erro original `error` é passado.
+   * Cria um toast pelo serviço `ToastService` com as informações provenientes do mapa `ToastErrorInfoMap`. Caso o erro não esteja mapeado,
+   * apenas registra o erro no console.
+   *
+   * Ao momento que o toast expirar ou ser fechado pelo usuário,
+   * recebe notificação do `Subject` do serviço e executa métodos opcionais de acordo com o erro, providos em `_handleErrorOptions`.
    *
    * @param {HttpErrorResponse} error - O erro fornecido pelo seletor do operador RxJS `catchError`.
    */
   public handleError(error: HttpErrorResponse): void {
-    const errorHandleFunction =
-      this._errorHandleFnMap[this._errorCodesList[error.status]] ??
-      this.fallbackFuncion;
+    const toastErrorInfo = ToastErrorInfoMap[error.status] ?? undefined;
 
-    errorHandleFunction.apply(this, [error]);
+    if (toastErrorInfo) {
+      this._toastService.showToast(toastErrorInfo);
+
+      const handleErrorOptionsFunction =
+        this._handleErrorOptions[this._errorCodesList[error.status]];
+
+      this._toastService.toastNotifier$.subscribe((value) => {
+        if (value) {
+          handleErrorOptionsFunction.apply(this);
+        }
+      });
+    } else {
+      console.log(
+        `Tratamento de erro para ${error.status}: ${
+          this._errorCodesList[error.status]
+        } inexistente.`
+      );
+      console.log(error);
+    }
   }
 
   /**
    * @private
-   * Método que lida com o erro 401 - `Unauthorized`.
-   *
-   * Armazena o caminho local para redirecionar o usuário após ele refazer o login.
-   *
-   * @param {HttpErrorResponse} errorArg - Argumento local de erro, substituido pelo erro original da chamada `Function.apply()` de `handleError()`.
+   * Navega o usuário para a página principal.
    */
-  private handleUnauthorized(errorArg: HttpErrorResponse): void {
-    this._toastNotifierService.notifyToast('error', errorArg.status);
+  private handleBadRequestOptions(): void {
+    this._router.navigateByUrl('main');
+  }
 
+  /**
+   * @private
+   * Armazena a url atual em `sessionStorage` e navega o usuário para a página de login.
+   * Lógica de redirecionamento é aplicada no componente `AuthRedirectComponent`.
+   */
+  private handleUnauthorizedOptions(): void {
     sessionStorage.setItem('currentUrl', this._router.url);
-
-    this._toastNotifierService.redirectOnToastClose(this._router, 'login');
+    this._router.navigateByUrl('login');
   }
 
   /**
    * @private
-   * Método que lida com o erro 500 - `Internal Server Error`.
-   *
-   * Apenas notifica o usuário do erro através de um toast e o redireciona para a página principal.
-   *
-   * @param {HttpErrorResponse} errorArg - Argumento local de erro, substituido pelo erro original da chamada `Function.apply()` de `handleError()`.
+   * Navega o usuário para a página principal.
    */
-  private handleInternalServerError(errorArg: HttpErrorResponse): void {
-    this._toastNotifierService.notifyToast('error', errorArg.status);
-
-    this._toastNotifierService.redirectOnToastClose(this._router, 'main');
-  }
-
-  /**
-   * @private
-   * Método que lida com o erro 400 - `Bad Request`.
-   *
-   * Apenas notifica o usuário do erro através de um toast e o redireciona para a página principal.
-   *
-   * @param {HttpErrorResponse} errorArg - Argumento local de erro, substituido pelo erro original da chamada `Function.apply()` de `handleError()`.
-   */
-  private handleBadRequest(errorArg: HttpErrorResponse): void {
-    this._toastNotifierService.notifyToast('error', errorArg.status);
-
-    this._toastNotifierService.redirectOnToastClose(this._router, 'main');
-  }
-
-  /**
-   * @private
-   * Método de fallback caso o erro fornecido não seja contemplado em `errorHandleFnMap`.
-   *
-   * @param {HttpErrorResponse} errorArg - Argumento local de erro, substituido pelo erro original da chamada `Function.apply()` de `handleError()`.
-   */
-  private fallbackFuncion(errorArg: HttpErrorResponse): void {
-    console.log(
-      `Tratamento de erro para ${errorArg.status}: ${
-        this._errorCodesList[errorArg.status]
-      } inexistente.`
-    );
-    console.log(errorArg);
+  private handleInternalServerErrorOptions(): void {
+    this._router.navigateByUrl('main');
   }
 }
