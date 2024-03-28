4ad91ea09f83b9d562446a7a753f5babc410d3fa
Artur Faria <artur.faria.1992@gmail.com>
2024-03-28 16:15:12 -0300

    ref: change toast-info-map to include type to toast info

--- src/app/shared/utils/toast-info-map.ts
+++ src/app/shared/utils/toast-info-map.ts
@@ -1,29 +1,28 @@
-interface IToastComponentInfo {
-  header: string;
-  body: string;
-  delay?: number;
+import { IToastInfo } from '../services/toast/toast.service';
+
+interface ToastErrorInfo {
+  [code: number]: IToastInfo;
 }
 
-type ToastErrorInfo = {
-  [code: number]: IToastComponentInfo;
-};
-
-type ToastSuccessInfo = {
+interface ToastSuccessInfo {
   [source: string]: {
-    [method: string]: Pick<IToastComponentInfo, 'body' | 'delay'>;
+    [method: string]: IToastInfo;
   };
-};
+}
 
 export const ToastErrorInfoMap: ToastErrorInfo = {
   400: {
+    type: 'error',
     header: 'Erro 400 - Requisição Inválida',
     body: 'O corpo da requisição possui erros. Por favor, contate a equipe técnica da aplicação',
   },
   401: {
+    type: 'error',
     header: 'Erro 401 - Não Autorizado',
     body: 'O token de autenticação não é válido. O sistema será redirecionado para a página de login para tentar a autenticação novamente. Ao completar o processo, você retornará para a página que estava anteriormente.',
   },
   500: {
+    type: 'error',
     header: 'Erro 500 - Erro Interno de Servidor',
     body: 'Houve um problema ao tentar processar a requisição. Por favor, contate a equipe técnica da aplicação.',
   },
@@ -31,18 +30,54 @@ export const ToastErrorInfoMap: ToastErrorInfo = {
 
 export const ToastSuccessInfoMap: ToastSuccessInfo = {
   Projeto: {
-    POST: { body: 'Projeto cadastrado com sucesso.' },
-    PUT: { body: 'Projeto atualizado com sucesso.' },
-    DELETE: { body: 'Projeto excluído com sucesso.' },
+    POST: {
+      type: 'success',
+      header: 'Sucesso',
+      body: 'Projeto cadastrado com sucesso.',
+    },
+    PUT: {
+      type: 'success',
+      header: 'Sucesso',
+      body: 'Projeto atualizado com sucesso.',
+    },
+    DELETE: {
+      type: 'success',
+      header: 'Sucesso',
+      body: 'Projeto excluído com sucesso.',
+    },
   },
   Pessoa: {
-    POST: { body: 'Pessoa cadastrada com sucesso.' },
-    PUT: { body: 'Pessoa atualizada com sucesso.' },
-    DELETE: { body: 'Pessoa excluída com sucesso.' },
+    POST: {
+      type: 'success',
+      header: 'Sucesso',
+      body: 'Pessoa cadastrada com sucesso.',
+    },
+    PUT: {
+      type: 'success',
+      header: 'Sucesso',
+      body: 'Pessoa atualizada com sucesso.',
+    },
+    DELETE: {
+      type: 'success',
+      header: 'Sucesso',
+      body: 'Pessoa excluída com sucesso.',
+    },
   },
   Organização: {
-    POST: { body: 'Organização cadastrada com sucesso.' },
-    PUT: { body: 'Organização atualizada com sucesso.' },
-    DELETE: { body: 'Organização excluída com sucesso.' },
+    POST: {
+      type: 'success',
+      header: 'Sucesso',
+      body: 'Organização cadastrada com sucesso.',
+    },
+    PUT: {
+      type: 'success',
+      header: 'Sucesso',
+      body: 'Organização atualizada com sucesso.',
+    },
+    DELETE: {
+      type: 'success',
+      header: 'Sucesso',
+      body: 'Organização excluída com sucesso.',
+    },
   },
 };
