bc1cbf18add44619f3bf18911085b01abe4f1692
Artur Faria <artur.faria.1992@gmail.com>
2024-04-01 15:14:27 -0300

    ref: change table-action-buttons component

--- src/app/core/templates/table-action-buttons/table-action-buttons.component.ts
+++ src/app/core/templates/table-action-buttons/table-action-buttons.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { ITableActionsDataInput } from '../../../shared/interfaces/table-actions-data-input.interface';
 
 @Component({
   selector: 'table-action-buttons-template',
@@ -6,4 +18,43 @@ import { Component } from '@angular/core';
   templateUrl: './table-action-buttons.component.html',
   styleUrl: './table-action-buttons.component.scss',
 })
-export class TableActionButtonsComponent {}
+export class TableActionButtonsComponent {
+  @ViewChild('deleteModalTemplate')
+  deleteModalTemplate!: TemplateRef<NgbActiveModal>;
+
+  @Input('rowData') rowData!: ITableActionsDataInput;
+  @Output('deleteId') deleteId: EventEmitter<number> =
+    new EventEmitter<number>();
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _modalService: NgbModal
+  ) {}
+
+  private tableNavigation(data: ITableActionsDataInput, formMode: string) {
+    this._router.navigate(['form', formMode], {
+      relativeTo: this._route,
+      queryParams: { id: data.id },
+    });
+  }
+
+  public tableDetails(data: ITableActionsDataInput) {
+    this.tableNavigation(data, 'detalhes');
+  }
+
+  public tableEdit(data: ITableActionsDataInput) {
+    this.tableNavigation(data, 'editar');
+  }
+
+  public tableDelete(data: ITableActionsDataInput) {
+    const deleteModalRef = this._modalService.open(this.deleteModalTemplate);
+
+    deleteModalRef.result.then(
+      (deleteId) => {
+        this.deleteId.emit(deleteId);
+      },
+      () => {}
+    );
+  }
+}
