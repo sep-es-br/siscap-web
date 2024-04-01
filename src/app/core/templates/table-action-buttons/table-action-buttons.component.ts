01a54c731ed21fc30dc5f45c3b53212654eb17fc
Artur Faria <artur.faria.1992@gmail.com>
2024-04-01 13:33:04 -0300

    feat: add table-action-buttons component

---
+++ src/app/core/templates/table-action-buttons/table-action-buttons.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'table-action-buttons-template',
+  standalone: false,
+  templateUrl: './table-action-buttons.component.html',
+  styleUrl: './table-action-buttons.component.scss',
+})
+export class TableActionButtonsComponent {}
