ac8159dca029af2c39d54c2a9cfb876753af9b08
Artur Faria <artur.faria.1992@gmail.com>
2024-04-04 11:48:31 -0300

    feat: add directives module to imports and exports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { ComponentsModule } from './components/components.module';
 import { PipesModule } from './pipes/pipes.module';
 import { TemplatesModule } from './templates/templates.module';
+import { DirectivesModule } from './directives/directives.module';
 
 @NgModule({
-  imports: [ComponentsModule, PipesModule, TemplatesModule],
-  exports: [ComponentsModule, PipesModule, TemplatesModule],
+  imports: [ComponentsModule, PipesModule, TemplatesModule, DirectivesModule],
+  exports: [ComponentsModule, PipesModule, TemplatesModule, DirectivesModule],
 })
 export class CoreModule {}
