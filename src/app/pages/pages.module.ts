81d6d85067938fb1dea478c5ffb98e148179f75c
Artur Faria <artur.faria.1992@gmail.com>
2024-04-05 16:53:59 -0300

    ref: add ngbtooltip module import; remove unused imports, reorganize imports

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 import { NgSelectModule } from '@ng-select/ng-select';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
-import { TableModule } from 'primeng/table';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+// import { TableModule } from 'primeng/table';
 
 import { CoreModule } from '../core/core.module';
+import { PagesRoutingModule } from './pages-routing.module';
+
 import { LoginComponent } from '../pages/login/login.component';
+import { AuthRedirectComponent } from './auth-redirect/auth-redirect.component';
+import { MainComponent } from './main/main.component';
 import { HomeComponent } from '../pages/home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectFormComponent } from './projects/form/project-form.component';
-import { MainComponent } from './main/main.component';
-import { PagesRoutingModule } from './pages-routing.module';
-import { AuthRedirectComponent } from './auth-redirect/auth-redirect.component';
 import { PersonsComponent } from './persons/persons.component';
 import { PersonFormComponent } from './persons/form/person-form.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
@@ -36,16 +38,17 @@ import { OrganizationFormComponent } from './organizations/form/organization-for
   ],
   imports: [
     CommonModule,
-    RouterOutlet,
-    CoreModule,
+    FormsModule,
     ReactiveFormsModule,
-    TableModule,
-    HttpClientModule,
+    RouterOutlet,
     NgSelectModule,
     NgxMaskDirective,
     NgxMaskPipe,
-    FormsModule,
+    NgbTooltipModule,
+    CoreModule,
     PagesRoutingModule,
+    // TableModule,
+    // HttpClientModule,
   ],
   providers: [provideNgxMask()],
   exports: [
