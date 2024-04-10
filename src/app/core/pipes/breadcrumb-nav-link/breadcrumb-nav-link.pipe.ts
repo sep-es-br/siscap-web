eac528df9a77447b713344e5586ed8222860be5c
Artur Faria <artur.faria.1992@gmail.com>
2024-04-10 13:25:31 -0300

    ref: capitalize pipe class name; comment details enum properties

--- src/app/core/pipes/breadcrumb-nav-link/breadcrumb-nav-link.pipe.ts
+++ src/app/core/pipes/breadcrumb-nav-link/breadcrumb-nav-link.pipe.ts
@@ -7,26 +7,26 @@ enum breadCrumbTitles {
 
   projetoscriar = 'Novo Projeto',
   projetoseditar = 'Editar Projeto',
-  projetosdetalhes = 'Visualizar Projeto',
+  // projetosdetalhes = 'Visualizar Projeto',
 
   pessoas = 'Pessoas',
 
   pessoascriar = 'Novo Usuário',
   pessoaseditar = 'Editar Pessoa',
-  pessoasdetalhes = 'Visualizar Pessoa',
+  // pessoasdetalhes = 'Visualizar Pessoa',
 
   organizacoes = 'Organizações',
 
   organizacoescriar = 'Nova Organização',
   organizacoeseditar = 'Editar Organização',
-  organizacoesdetalhes = 'Visualizar Organização',
+  // organizacoesdetalhes = 'Visualizar Organização',
 }
 
 @Pipe({
   name: 'breadcrumbnavlink',
   standalone: true,
 })
-export class BreadcrumbnavlinkPipe implements PipeTransform {
+export class BreadcrumbNavLinkPipe implements PipeTransform {
   transform(value: string, ...args: unknown[]): string {
     return breadCrumbTitles[value as keyof typeof breadCrumbTitles];
   }
