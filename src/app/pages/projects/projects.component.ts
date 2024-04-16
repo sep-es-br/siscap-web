2bf58a77f784597892c47317593563ba9e155cbf
Artur Faria <artur.faria.1992@gmail.com>
2024-04-16 12:26:19 -0300

    ref: remove comments and unused imports

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subscription, first, tap } from 'rxjs';
 
 import { ProjetosService } from '../../shared/services/projetos/projetos.service';
-import { ToastService } from '../../shared/services/toast/toast.service';
 
 import { SortColumn } from '../../core/directives/sortable/sortable.directive';
 
@@ -12,7 +11,6 @@ import {
   IProjectGet,
   IProjectTable,
 } from '../../shared/interfaces/project.interface';
-import { ITableActionsDataInput } from '../../shared/interfaces/table-actions-data-input.interface';
 
 import { sortTableColumnsFunction } from '../../shared/utils/sort-table-columns-function';
 
@@ -24,7 +22,6 @@ import { sortTableColumnsFunction } from '../../shared/utils/sort-table-columns-
 })
 export class ProjectsComponent implements OnInit, OnDestroy {
   private _getProjetos$: Observable<IProjectGet>;
-  private _deleteProjeto$!: Observable<string>;
 
   private _subscription: Subscription = new Subscription();
 
@@ -34,7 +31,6 @@ export class ProjectsComponent implements OnInit, OnDestroy {
     private _router: Router,
     private _route: ActivatedRoute,
     private _projetosService: ProjetosService,
-    private _toastService: ToastService
   ) {
     this._getProjetos$ = this._projetosService.getProjetos().pipe(
       first(),
@@ -63,38 +59,6 @@ export class ProjectsComponent implements OnInit, OnDestroy {
       queryParams: { id: project.id },
     });
   }
-
-  // public projetoDataInput(project: IProjectTable): ITableActionsDataInput {
-  //   const projetoDataInput: ITableActionsDataInput = {
-  //     id: project.id,
-  //     infoTitle:
-  //       'O seguinte projeto será excluído. Tem certeza que quer executar a ação?',
-  //     infoBody: {
-  //       Sigla: project.sigla,
-  //       'Nome do Projeto': project.titulo,
-  //     },
-  //   };
-
-  //   return projetoDataInput;
-  // }
-
-  // public deleteProjeto(id: number) {
-  //   this._deleteProjeto$ = this._projetosService.deleteProjeto(id).pipe(
-  //     tap((response) => {
-  //       if (response) {
-  //         this._toastService.showToast(
-  //           'success',
-  //           'Projeto excluído com sucesso.'
-  //         );
-  //         this._router
-  //           .navigateByUrl('/', { skipLocationChange: true })
-  //           .then(() => this._router.navigateByUrl('main/projetos'));
-  //       }
-  //     })
-  //   );
-
-  //   this._subscription.add(this._deleteProjeto$.subscribe());
-  // }
 
   queryProject() {}
 
