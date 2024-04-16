39c357d826e9e0b189fc153cd781c5a94adaa977
Artur Faria <artur.faria.1992@gmail.com>
2024-04-16 11:19:45 -0300

    ref: remove comments

--- src/app/core/pipes/breadcrumb-nav-link/breadcrumb-nav-link.pipe.ts
+++ src/app/core/pipes/breadcrumb-nav-link/breadcrumb-nav-link.pipe.ts
@@ -7,19 +7,16 @@ enum breadCrumbTitles {
 
   projetoscriar = 'Novo Projeto',
   projetoseditar = 'Editar Projeto',
-  // projetosdetalhes = 'Visualizar Projeto',
 
   pessoas = 'Pessoas',
 
   pessoascriar = 'Novo Usuário',
   pessoaseditar = 'Editar Pessoa',
-  // pessoasdetalhes = 'Visualizar Pessoa',
 
   organizacoes = 'Organizações',
 
   organizacoescriar = 'Nova Organização',
   organizacoeseditar = 'Editar Organização',
-  // organizacoesdetalhes = 'Visualizar Organização',
 }
 
 @Pipe({
