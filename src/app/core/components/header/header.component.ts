8af261888bb87b8846f73bbc1335b2be0929de43
Artur Faria <artur.faria.1992@gmail.com>
2024-04-17 14:57:19 -0300

    ref: remove getPessoaByEmail logic from component; change redirectUserProfile method navigate queryParams object from id to email

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,14 +1,9 @@
 import { Component, Input, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { tap } from 'rxjs';
-
 import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
-import { PessoasService } from '../../../shared/services/pessoas/pessoas.service';
-
 import { IProfile } from '../../../shared/interfaces/profile.interface';
-import { IPerson } from '../../../shared/interfaces/person.interface';
 
 @Component({
   selector: 'siscap-header',
@@ -19,27 +14,15 @@ import { IPerson } from '../../../shared/interfaces/person.interface';
 export class HeaderComponent {
   @Input('offcanvasRef') navmenuOffcanvas!: TemplateRef<any>;
 
-  private userId!: number;
-
   public userProfile!: IProfile;
 
   constructor(
     private _router: Router,
-    private _pessoasService: PessoasService,
     private _offcanvasService: NgbOffcanvas
   ) {
     if (!!sessionStorage.getItem('user-profile')) {
       this.userProfile = JSON.parse(sessionStorage.getItem('user-profile')!);
     }
-
-    this._pessoasService
-      .getPessoaByEmail(this.userProfile.email)
-      .pipe(
-        tap((response: IPerson) => {
-          this.userId = response.id;
-        })
-      )
-      .subscribe();
   }
 
   showOffcanvas() {
@@ -52,7 +35,7 @@ export class HeaderComponent {
 
   redirectUserProfile() {
     this._router.navigate(['main', 'pessoas', 'form', 'editar'], {
-      queryParams: { id: this.userId },
+      queryParams: { email: this.userProfile.email },
     });
   }
 
