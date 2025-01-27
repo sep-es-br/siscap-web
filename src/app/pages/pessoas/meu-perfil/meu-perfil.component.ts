ae4dbd30bdb0d104ef2c201d1d65ac826f8dfa1d
Artur Faria <artur.faria.1992@gmail.com>
2025-01-27 11:52:27 -0300

    ref: remove campos 'cpf', 'genero' e 'endereco' da entidade Pessoa, altera componentes pertinentes

--- src/app/pages/pessoas/meu-perfil/meu-perfil.component.ts
+++ src/app/pages/pessoas/meu-perfil/meu-perfil.component.ts
@@ -30,7 +30,7 @@ import { NavegacaoService } from '../../../core/services/navegacao/navegacao.ser
 import { ToastService } from '../../../core/services/toast/toast.service';
 
 import {
-  EnderecoModel,
+  // EnderecoModel,
   PessoaFormModel,
   PessoaModel,
 } from '../../../core/models/pessoa.model';
@@ -41,10 +41,10 @@ import {
   IPessoaForm,
 } from '../../../core/interfaces/pessoa.interface';
 
-import {
-  EnderecoFormType,
-  EnderecoFormTypeValue,
-} from '../../../core/types/form/endereco-form.type';
+// import {
+//   EnderecoFormType,
+//   EnderecoFormTypeValue,
+// } from '../../../core/types/form/endereco-form.type';
 
 import {
   BotoesConfig,
@@ -64,7 +64,7 @@ import {
   alterarEstadoControlesFormulario,
   converterArrayBufferEmImgSrc,
 } from '../../../core/utils/functions';
-import { CPFValidator } from '../../../core/validators/cpf.validator';
+// import { CPFValidator } from '../../../core/validators/cpf.validator';
 
 @Component({
   selector: 'siscap-meu-perfil',
@@ -98,7 +98,7 @@ export class MeuPerfilComponent implements OnInit, OnDestroy {
 
   // Por hora, lista de valores hard-coded
   public nacionalidadesList: Array<string> = LISTA_NACIONALIDADES;
-  public generosList: Array<string> = LISTA_GENEROS;
+  // public generosList: Array<string> = LISTA_GENEROS;
 
   public srcImagemPessoa: string = '';
   public arquivoImagemPessoa: File | undefined;
@@ -201,16 +201,16 @@ export class MeuPerfilComponent implements OnInit, OnDestroy {
       nacionalidade: this._nnfb.control(pessoaFormModel.nacionalidade, {
         validators: Validators.required,
       }),
-      genero: this._nnfb.control(pessoaFormModel.genero, {
-        validators: Validators.required,
-      }),
-      cpf: this._nnfb.control(pessoaFormModel.cpf, {
-        validators: [
-          Validators.minLength(11),
-          Validators.maxLength(11),
-          CPFValidator,
-        ],
-      }),
+      // genero: this._nnfb.control(pessoaFormModel.genero, {
+      //   validators: Validators.required,
+      // }),
+      // cpf: this._nnfb.control(pessoaFormModel.cpf, {
+      //   validators: [
+      //     Validators.minLength(11),
+      //     Validators.maxLength(11),
+      //     CPFValidator,
+      //   ],
+      // }),
       email: this._nnfb.control(pessoaFormModel.email, {
         validators: [Validators.required, Validators.email],
       }),
@@ -218,97 +218,93 @@ export class MeuPerfilComponent implements OnInit, OnDestroy {
       telefonePessoal: this._nnfb.control(pessoaFormModel.telefonePessoal),
       idOrganizacoes: this._nnfb.control(pessoaFormModel.idOrganizacoes),
       idAreasAtuacao: this._nnfb.control(pessoaFormModel.idAreasAtuacao),
-      endereco: this.iniciarEnderecoForm(pessoaFormModel.endereco),
+      // endereco: this.iniciarEnderecoForm(pessoaFormModel.endereco),
     });
 
     this.meuPerfilFormValueChanges();
-    this.enderecoFormValueChanges();
+    // this.enderecoFormValueChanges();
   }
 
-  private iniciarEnderecoForm(
-    enderecoModel?: EnderecoModel
-  ): FormGroup<EnderecoFormType> {
-    return this._nnfb.group({
-      rua: this._nnfb.control(enderecoModel?.rua ?? null),
-      numero: this._nnfb.control(enderecoModel?.numero ?? null),
-      bairro: this._nnfb.control(enderecoModel?.bairro ?? null),
-      complemento: this._nnfb.control(enderecoModel?.complemento ?? null),
-      codigoPostal: this._nnfb.control(enderecoModel?.codigoPostal ?? null),
-      idPais: this._nnfb.control(enderecoModel?.idPais ?? null),
-      idEstado: this._nnfb.control(enderecoModel?.idEstado ?? null),
-      idCidade: this._nnfb.control(enderecoModel?.idCidade ?? null),
-    });
-  }
+  // private iniciarEnderecoForm(
+  //   enderecoModel?: EnderecoModel
+  // ): FormGroup<EnderecoFormType> {
+  //   return this._nnfb.group({
+  //     rua: this._nnfb.control(enderecoModel?.rua ?? null),
+  //     numero: this._nnfb.control(enderecoModel?.numero ?? null),
+  //     bairro: this._nnfb.control(enderecoModel?.bairro ?? null),
+  //     complemento: this._nnfb.control(enderecoModel?.complemento ?? null),
+  //     codigoPostal: this._nnfb.control(enderecoModel?.codigoPostal ?? null),
+  //     idPais: this._nnfb.control(enderecoModel?.idPais ?? null),
+  //     idEstado: this._nnfb.control(enderecoModel?.idEstado ?? null),
+  //     idCidade: this._nnfb.control(enderecoModel?.idCidade ?? null),
+  //   });
+  // }
 
   private meuPerfilFormValueChanges(): void {
-    const endereco_idPaisFormControl = this.meuPerfilForm.get(
-      'endereco.idPais'
-    ) as FormControl<number | null>;
-
-    const endereco_idEstadoFormControl = this.meuPerfilForm.get(
-      'endereco.idEstado'
-    ) as FormControl<number | null>;
-
-    const endereco_idCidadeFormControl = this.meuPerfilForm.get(
-      'endereco.idCidade'
-    ) as FormControl<number | null>;
-
-    endereco_idPaisFormControl.valueChanges.subscribe((idPaisValue) => {
-      if (!idPaisValue) {
-        endereco_idEstadoFormControl.patchValue(null);
-        endereco_idCidadeFormControl.patchValue(null);
-        this.estadosOpcoes = [];
-        this.cidadesOpcoes = [];
-      } else {
-        this._opcoesDropdownService
-          .getOpcoesEstados(idPaisValue)
-          .pipe(tap((response) => (this.estadosOpcoes = response)))
-          .subscribe();
-      }
-    });
-
-    endereco_idEstadoFormControl.valueChanges.subscribe((idEstadoValue) => {
-      if (!idEstadoValue) {
-        endereco_idCidadeFormControl.patchValue(null);
-        this.cidadesOpcoes = [];
-      } else {
-        this._opcoesDropdownService
-          .getOpcoesCidades('ESTADO', idEstadoValue)
-          .pipe(tap((response) => (this.cidadesOpcoes = response)))
-          .subscribe();
-      }
-    });
+    // const endereco_idPaisFormControl = this.meuPerfilForm.get(
+    //   'endereco.idPais'
+    // ) as FormControl<number | null>;
+    // const endereco_idEstadoFormControl = this.meuPerfilForm.get(
+    //   'endereco.idEstado'
+    // ) as FormControl<number | null>;
+    // const endereco_idCidadeFormControl = this.meuPerfilForm.get(
+    //   'endereco.idCidade'
+    // ) as FormControl<number | null>;
+    // endereco_idPaisFormControl.valueChanges.subscribe((idPaisValue) => {
+    //   if (!idPaisValue) {
+    //     endereco_idEstadoFormControl.patchValue(null);
+    //     endereco_idCidadeFormControl.patchValue(null);
+    //     this.estadosOpcoes = [];
+    //     this.cidadesOpcoes = [];
+    //   } else {
+    //     this._opcoesDropdownService
+    //       .getOpcoesEstados(idPaisValue)
+    //       .pipe(tap((response) => (this.estadosOpcoes = response)))
+    //       .subscribe();
+    //   }
+    // });
+    // endereco_idEstadoFormControl.valueChanges.subscribe((idEstadoValue) => {
+    //   if (!idEstadoValue) {
+    //     endereco_idCidadeFormControl.patchValue(null);
+    //     this.cidadesOpcoes = [];
+    //   } else {
+    //     this._opcoesDropdownService
+    //       .getOpcoesCidades('ESTADO', idEstadoValue)
+    //       .pipe(tap((response) => (this.cidadesOpcoes = response)))
+    //       .subscribe();
+    //   }
+    // });
   }
 
-  private enderecoFormValueChanges(): void {
-    const enderecoForm = this.meuPerfilForm.get(
-      'endereco'
-    ) as FormGroup<EnderecoFormType>;
+  // private enderecoFormValueChanges(): void {
+  //   const enderecoForm = this.meuPerfilForm.get(
+  //     'endereco'
+  //   ) as FormGroup<EnderecoFormType>;
 
-    enderecoForm.markAllAsTouched();
+  //   enderecoForm.markAllAsTouched();
 
-    enderecoForm.valueChanges
-      .pipe(
-        distinctUntilChanged(
-          (prev, curr) => JSON.stringify(prev) === JSON.stringify(curr)
-        ),
-        map<EnderecoFormTypeValue, boolean>((enderecoFormValue) =>
-          Object.values(enderecoFormValue).some((value) => !!value)
-        ),
-        tap((resultado) => {
-          for (const key in enderecoForm.controls) {
-            const control = enderecoForm.get(key);
+  //   enderecoForm.valueChanges
+  //     .pipe(
+  //       distinctUntilChanged(
+  //         (prev, curr) => JSON.stringify(prev) === JSON.stringify(curr)
+  //       ),
+  //       map<EnderecoFormTypeValue, boolean>((enderecoFormValue) =>
+  //         Object.values(enderecoFormValue).some((value) => !!value)
+  //       ),
+  //       tap((resultado) => {
+  //         for (const key in enderecoForm.controls) {
+  //           const control = enderecoForm.get(key);
 
-            resultado
-              ? control?.setValidators(Validators.required)
-              : control?.clearValidators();
+  //           resultado
+  //             ? control?.setValidators(Validators.required)
+  //             : control?.clearValidators();
 
-            control?.updateValueAndValidity({ emitEvent: false });
-          }
-        })
-      )
-      .subscribe();
-  }
+  //           control?.updateValueAndValidity({ emitEvent: false });
+  //         }
+  //       })
+  //     )
+  //     .subscribe();
+  // }
 
   private dispararModalAtencao(nomeOrganizacao: string): void {
     const modalRef = this._ngbModalService.open(
@@ -347,11 +343,11 @@ export class MeuPerfilComponent implements OnInit, OnDestroy {
 
     alterarEstadoControlesFormulario(permitir, meuPerfilFormControls);
 
-    const enderecoFormControls = (
-      this.meuPerfilForm.get('endereco') as FormGroup<EnderecoFormType>
-    ).controls;
+    // const enderecoFormControls = (
+    //   this.meuPerfilForm.get('endereco') as FormGroup<EnderecoFormType>
+    // ).controls;
 
-    alterarEstadoControlesFormulario(permitir, enderecoFormControls);
+    // alterarEstadoControlesFormulario(permitir, enderecoFormControls);
 
     // Caso específico de email:
     this.meuPerfilForm.get('email')?.disable();
