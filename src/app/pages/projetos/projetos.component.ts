98e0730c683b1ee1af68a0f1ce4cea785e953fa8
Eduardo Simonato <ersimonato.es@gmail.com>
2025-11-13 10:50:29 -0300

    commit inicial - corrigir tratamento para carregamento de pessoas do orgao de origem;

--- src/app/pages/projetos/projetos.component.ts
+++ src/app/pages/projetos/projetos.component.ts
@@ -87,8 +87,7 @@ export class ProjetosComponent implements OnInit, OnDestroy {
 
     this._subscription.add(
       this._projetosService.atualizarListaProjetos$.subscribe(() => {
-        // console.log('recarregando lista de DIC´s.. ')
-        this.fetchPage(); // 👈 recarrega a lista qdo houver acionamento pelo projetoservice
+        this.fetchPage();
       })
     );
 
