ffc96b3bec30fd995f14a45d4e0cbf59f8c8d39d
Ricardo Souza <rr_souza@outlook.com>
2026-03-31 18:10:22 -0300

    Mudanças

--- src/app/core/enums/status-programa.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum StatusProgramaEnum {
-  EDICAO = 'Edição',
-  AGUARDANDO_ASSINATURAS = 'Aguardando Assinaturas',
-  ASSINADO = 'Assinado',
-  AUTUADO = 'Autuado',
-  RECUSADO = 'Recusado',
-}
--- src/app/pages/programas/search/programas-search.component.ts
+++ src/app/pages/programas/search/programas-search.component.ts
@@ -1,9 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { IProgramaFiltroPesquisa } from '../../../core/interfaces/programa.interface';
+import { IProgramaFiltroPesquisa, StatusPrograma, StatusProgramaLabel } from '../../../core/interfaces/programa.interface';
 import { FormControl, FormGroup } from '@angular/forms';
 import { debounceTime } from 'rxjs';
 import { TEMPO_INPUT_USUARIO } from '../../../core/utils/constants';
-import { StatusProgramaEnum } from '../../../core/enums/status-programa.enum';
 
 @Component({
   selector: 'siscap-programas-search',
@@ -15,12 +14,19 @@ export class ProgramasSearchComponent {
 
   programasPesquisaForm!: FormGroup;
 
-  programasStatusOpcoes = ['Status', ...Object.values(StatusProgramaEnum)];
+  programasStatusOpcoes: Array<{ value: StatusPrograma; label: string; }> = 
+    Object
+      .entries(StatusProgramaLabel)
+      .map(([key, label]) => ({
+        value: Number(key),
+        label,
+      })
+  );
 
   constructor() {
     this.programasPesquisaForm = new FormGroup({
       status: new FormControl('Status'),
-      search: new FormControl(''),
+      porTermo: new FormControl(''),
     });
 
     this.programasPesquisaForm.valueChanges
