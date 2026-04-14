a509ef8b58eddb3f5b22fd963c05d849bdd79695
Eduardo Simonato <eduardo.simonato@pdcase.com.br>
2026-04-14 20:47:00 -0300

    commit ajustes task;

---
+++ src/app/pages/projeto-indicadores/indicador-projeto/indicador-projeto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-indicador-projeto',
+  standalone: true,
+  imports: [],
+  templateUrl: './indicador-projeto.component.html',
+  styleUrl: './indicador-projeto.component.scss'
+})
+export class IndicadorProjetoComponent {
+
+}
