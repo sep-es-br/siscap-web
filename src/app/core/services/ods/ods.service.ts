b4b84e0c85d346b0cf60a99f097ffb816644c815
Eduardo Simonato <eduardo.simonato@pdcase.com.br>
2026-06-02 14:06:47 -0300

    commit corrigir referencia a arquivo enviroment que estava errado.

--- src/app/core/services/ods/ods.service.ts
+++ src/app/core/services/ods/ods.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IOdsGestao } from '../../interfaces/ods-gestao.interface';
